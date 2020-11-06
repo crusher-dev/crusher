b2ce6e8385e725ae7814ae1631f6de824e58ece3
Utkarsh <utkarshdix02@gmail.com>
2020-11-07 01:20:29 +0530

    Add pm2 manager for crusher-app, crusher-server, crusher-extension and test-runner

--- packages/crusher-server/config/database.ts
+++ packages/crusher-server/config/database.ts
@@ -1,7 +1,7 @@
 export const REDDIS = {
-	host: process.env.REDDIS_HOST ? process.env.REDDIS_HOST : 'redis',
-	port: process.env.REDDIS_PORT ? process.env.REDDIS_PORT : 6379,
-	password: process.env.REDDIS_PASSWORD ? process.env.REDDIS_PASSWORD : null,
+	host: process.env.REDIS_HOST ? process.env.REDIS_HOST : 'redis',
+	port: process.env.REDIS_PORT ? process.env.REDIS_PORT : 6379,
+	password: process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : null,
 };
 
 export const MONGODB = {
@@ -9,7 +9,7 @@ export const MONGODB = {
 	host: process.env.MONGODB_HOST ? process.env.MONGODB_HOST : 'localhost',
 	port: process.env.MONGODB_PORT ? process.env.MONGODB_PORT : 27017,
 	// username: process.env.MONGODB_USERNAME ? process.env.MONGODB_USERNAME : 'remote',
-	username: 'remote',
+	username: process.env.MONGODB_USERNAME ? process.env.MONGODB_USERNAME : 'remote',
 	password: process.env.MONGODB_PASSWORD ? process.env.MONGODB_PASSWORD : 'password',
 	database: process.env.MONGODB_DATABASE ? process.env.MONGODB_DATABASE : 'crusher',
 };
--- packages/test-runner/config/database.ts
+++ packages/test-runner/config/database.ts
@@ -1,15 +1,15 @@
 export const REDDIS = {
-	host: process.env.REDDIS_HOST ? process.env.REDDIS_HOST : "192.168.99.104",
+	host: process.env.REDIS_HOST ? process.env.REDIS_HOST : "localhost",
 	//@ts-ignore
-	port: process.env.REDDIS_PORT ? parseInt(process.env.REDDIS_PORT) : 6379,
-	password: process.env.REDDIS_PASSWORD ?  process.env.REDDIS_PASSWORD : null
+	port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+	password: process.env.REDIS_PASSWORD ?  process.env.REDIS_PASSWORD : null
 };
 
 export const MONGODB = {
 	connectionString: process.env.MONGODB_CONNECTION_STRING ? process.env.MONGODB_CONNECTION_STRING : null,
-	host: process.env.MONGODB_HOST ? process.env.MONGODB_HOST : '192.168.99.104',
+	host: process.env.MONGODB_HOST ? process.env.MONGODB_HOST : 'localhost',
 	port: process.env.MONGODB_PORT ? process.env.MONGODB_PORT : 27000,
 	username: process.env.MONGODB_USERNAME ? process.env.MONGODB_USERNAME : 'remote',
 	password: process.env.MONGODB_PASSWORD ? process.env.MONGODB_PASSWORD : 'password',
-	database: process.env.MONGODB_DATABASE ? process.env.MONGODB_DATABASE : 'crusher'
+	database: process.env.MONGODB_DATABASE ? process.env.MONGODB_DATABASE : 'crusher',
 }

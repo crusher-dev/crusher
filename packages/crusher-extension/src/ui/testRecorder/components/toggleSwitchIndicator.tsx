4a04e682d2e2351397ff5827d85e10a9468af953
Utkarsh <utkarshdix02@gmail.com>
2020-11-12 09:39:32 +0530

    Fix issues

---
+++ packages/crusher-extension/src/ui/testRecorder/components/toggleSwitchIndicator.tsx
@@ -0,0 +1,60 @@
+import {useEffect, useRef} from "preact/hooks";
+import {Ref} from "preact";
+import React from "preact/compat";
+
+interface ToggleSwitchIndicatorProps {
+    label: string;
+    enabled: boolean;
+};
+
+interface ToggleIndicatorProps {
+    enabled: boolean;
+};
+
+const ToggleIndicator = (props: ToggleIndicatorProps) => {
+    const {enabled} = props;
+    const inputRef: Ref<HTMLInputElement> = useRef(null);
+
+    useEffect(() => {
+        inputRef.current!!.checked = enabled;
+    }, [enabled]);
+
+    return (
+        <div className="toggle-switch">
+            <input disabled ref={inputRef} type="checkbox" id="chkTest" name="chkTest"/>
+            <label htmlFor="chkTest">
+                <span className="toggle-track"></span>
+            </label>
+        </div>
+    );
+};
+
+const ToggleSwitchIndicator = (props: ToggleSwitchIndicatorProps) => {
+    const {label, enabled} = props;
+
+    return (
+        <div style={styles.toggleWithLabelContainer}>
+            <ToggleIndicator enabled={enabled}/>
+            <span style={styles.toggleLabel}>{label}</span>
+        </div>
+    )
+}
+
+const styles = {
+    toggleWithLabelContainer: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    toggleLabel: {
+        marginLeft: "1rem",
+        fontFamily: "DM Sans",
+        fontStyle: "normal",
+        fontWeight: "500",
+        fontSize: "0.7rem",
+        position: "relative",
+        color: "#fff",
+        top: "-0.1rem"
+    }
+};
+
+export {ToggleSwitchIndicator}

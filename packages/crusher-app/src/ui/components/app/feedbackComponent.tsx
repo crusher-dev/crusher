afd8ab76fde376a91b8472a4263287a256268e0b
Himanshu Dixit <hudixt@gmail.com>
2020-11-17 11:08:57 +0530

    Create feedback component

---
+++ packages/crusher-app/src/ui/components/app/feedbackComponent.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import {css} from "@emotion/core";
+
+const normalImageUrl = "/svg/dashboard/feedback.svg"
+const hoverImageUrl = "/svg/dashboard/feedback_hover.svg"
+export const FeedbackComponent = ()=>{
+    const [imgUrl, setImgUrl] = useState(normalImageUrl)
+    return(
+        <img src={imgUrl} css={feedbackCss} onMouseEnter={setImgUrl.bind(this,hoverImageUrl)} onMouseLeave={setImgUrl.bind(this,normalImageUrl)}/>
+    )
+}
+const feedbackCss = css`
+    aposition: fixed;
+    bottom: 0;
+    right: 0;
+    height: 6rem;
+    width: 10rem;
+    cursor: pointer;
+}
+`;

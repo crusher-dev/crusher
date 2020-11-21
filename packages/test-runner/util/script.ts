78924dbd38b8e00083f98fe156baed0bd89e4565
Himanshu Dixit <hudixt@gmail.com>
2020-11-21 17:14:38 +0530

    Fix feedback component

--- packages/test-runner/util/script.ts
+++ packages/test-runner/util/script.ts
@@ -8,27 +8,29 @@ export function replaceImportWithRequire(script: string) {
   if (!script) {
     return script;
   }
-  return script.replace(
-    matchImportRegex,
-    function (match, variableName, packageName) {
-      if (!variableName || !packageName) {
-        return match;
-      }
-      return `const ${variableName} = require(${packageName});`;
+  return script.replace(matchImportRegex, function (
+    match,
+    variableName,
+    packageName
+  ) {
+    if (!variableName || !packageName) {
+      return match;
     }
-  );
+    return `const ${variableName} = require(${packageName});`;
+  });
 }
 
 export function replaceWithPlaywrightWrapper(script: string, platform = null) {
-  return script.replace(
-    packageNameRequireMatchRegex,
-    function (match: string, quote: string, packageName: string) {
-      if (packageName === "playwright") {
-        return `require(${quote}../wrapper/playwright${quote}).boot(platform)`;
-      } else if (packageName === "playwright-video") {
-        return `require(${quote}../wrapper/playwright${quote})`;
-      }
-      return `require(${quote}${packageName}${quote})`;
+  return script.replace(packageNameRequireMatchRegex, function (
+    match: string,
+    quote: string,
+    packageName: string
+  ) {
+    if (packageName === "playwright") {
+      return `require(${quote}../wrapper/playwright${quote}).boot(platform)`;
+    } else if (packageName === "playwright-video") {
+      return `require(${quote}../wrapper/playwright${quote})`;
     }
-  );
+    return `require(${quote}${packageName}${quote})`;
+  });
 }

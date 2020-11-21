f6ced80e247cee8dd6840773c9b09b1781983cfa
Himanshu Dixit <hudixt@gmail.com>
2020-11-22 00:20:54 +0530

    Fix all packages

--- packages/test-runner/util/logs.ts
+++ packages/test-runner/util/logs.ts
@@ -3,15 +3,15 @@ let logs: Array<any> = [];
 
 // Override console.log to store all the logs in a var.
 console.log = function (text: any) {
-  logs.push({ message: text, time: new Date() });
-  __native__console__log(text);
+	logs.push({ message: text, time: new Date() });
+	__native__console__log(text);
 };
 
 module.exports = {
-  getLogs: function () {
-    return logs;
-  },
-  clearLogs: function () {
-    logs = [];
-  },
+	getLogs: function () {
+		return logs;
+	},
+	clearLogs: function () {
+		logs = [];
+	},
 };

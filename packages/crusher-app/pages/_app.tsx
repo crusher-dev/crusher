c08b367cbbd67c8f3da8f6d1d1af7b7a39b34074
Himanshu Dixit <hudixt@gmail.com>
2020-11-23 22:55:04 +0530

    Fix modal height

--- packages/crusher-app/pages/_app.tsx
+++ packages/crusher-app/pages/_app.tsx
@@ -35,6 +35,7 @@ function App({ Component, pageProps }) {
 						<PersistGate loading={null} persistor={(store as any).__persistor}>
 							<ThemeContext.Provider value={pageProps.theme}>
 								<Component {...pageProps} />
+
 							</ThemeContext.Provider>
 						</PersistGate>
 					</Provider>

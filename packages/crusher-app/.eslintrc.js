ee1099e099d8d496186c3347f610d7c38eea3cec
Utkarsh <utkarshdix02@gmail.com>
2020-11-29 01:26:40 +0530

    Fix css tsconfig

--- packages/crusher-app/.eslintrc.js
+++ packages/crusher-app/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
 		ecmaVersion: 12,
 		sourceType: "module",
 	},
-	plugins: ["react", "@typescript-eslint", "react-hooks"],
+	plugins: ["react", "@typescript-eslint", "react-hooks", "@emotion"],
 	rules: {
 		"linebreak-style": ["error", "unix"],
 		quotes: ["error", "double"],

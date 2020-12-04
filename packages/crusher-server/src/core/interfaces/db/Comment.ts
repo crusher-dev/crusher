efbc5dc8d3e0795abf4b67b283f52fcfbc3ed23a
Utkarsh <utkarshdix02@gmail.com>
2020-12-04 19:15:42 +0530

    Fix comments api and functionality

--- packages/crusher-server/src/core/interfaces/db/Comment.ts
+++ packages/crusher-server/src/core/interfaces/db/Comment.ts
@@ -3,10 +3,8 @@ import { BaseRowInterface } from './BaseRowInterface';
 export interface Comment extends BaseRowInterface {
 	id?: number;
 	user_id: number;
-	job_id: number;
-	instance_id: number;
-	screenshot_id: number;
-	result_set_id: number;
+	report_id: number;
+	result_id: number;
 	message: string;
 	replied_to?: number;
 }

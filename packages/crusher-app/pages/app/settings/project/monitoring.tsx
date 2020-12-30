3e1b6fa8bee0066bd08c1fd2b00490068964baa3
Viresh Uberoy <vireshuberoy01@gmail.com>
2020-12-30 09:24:56 +0530

    changed layout of code a bit

--- packages/crusher-app/pages/app/settings/project/monitoring.tsx
+++ packages/crusher-app/pages/app/settings/project/monitoring.tsx
@@ -4,6 +4,15 @@ import { PIXEL_REM_RATIO } from "@constants/other";
 import { SettingsContentHeader } from "@ui/components/settings/SettingsContentHeader";
 
 export default function monitoring() {
+	const trialObject: contentsOfModal = {
+		title: "Prod Monitoring",
+		host: "Production",
+		tags: "Production",
+		countries: "Screen/2020",
+		duration: "Every 10 sec",
+		escalation: "Production",
+	};
+
 	return (
 		<div css={monitoringCSS}>
 			<SettingsContent contentCSS={settingContentCSS}>
@@ -28,12 +37,18 @@ export default function monitoring() {
 	);
 }
 
-const monitoringCSS = css`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-`;
+interface contentsOfModal {
+	title: string;
+	host: string;
+	tags: string;
+	countries: string;
+	duration: string;
+	escalation: string;
+}
+
+interface iButtonProps {
+	onClick: () => void;
+}
 
 const Monitor = (props: iButtonProps) => {
 	const { onClick } = props;
@@ -47,79 +62,6 @@ const Monitor = (props: iButtonProps) => {
 const helloWorld = () => {
 	console.log("Hello World");
 };
-
-interface contentsOfModal {
-	title: string;
-	host: string;
-	tags: string;
-	countries: string;
-	duration: string;
-	escalation: string;
-}
-
-const buttonCSS = css`
-	background: #5286ff;
-	border-radius: ${4 / PIXEL_REM_RATIO}rem;
-	padding: ${8 / PIXEL_REM_RATIO}rem ${8 / PIXEL_REM_RATIO}rem;
-	min-width: ${180 / PIXEL_REM_RATIO}rem;
-	font-size: ${16 / PIXEL_REM_RATIO}rem;
-	font-weight: 600;
-	font-family: Gilroy;
-	color: #fff;
-	text-align: center;
-`;
-
-const trialObject: contentsOfModal = {
-	title: "Prod Monitoring",
-	host: "Production",
-	tags: "Production",
-	countries: "Screen/2020",
-	duration: "Every 10 sec",
-	escalation: "Production",
-};
-
-const modalCSS = css`
-	font-family: Gilroy;
-	background: #ffffff;
-	border: 2px solid #e6e6e6;
-	box-sizing: border-box;
-	border-radius: 8px;
-	display: flex;
-	flex-direction: column;
-	padding: 1.5rem;
-	margin: 1rem;
-	width: 45rem;
-`;
-
-const titleCSS = css`
-	font-family: Gilroy;
-	font-size: 1.2rem;
-	line-height: 1.25rem;
-	color: #323232;
-	padding: 0.5rem;
-	margin-top: 0rem;
-`;
-
-const keysCSS = css`
-	font-size: 0.9rem;
-	line-height: 1.125rem;
-	color: #9b9b9b;
-	padding: 0.5rem;
-`;
-
-const valuesCSS = css`
-	font-size: 0.9rem;
-	line-height: 1.125rem;
-	color: #323232;
-`;
-
-interface iButtonProps {
-	onClick: () => void;
-}
-
-const settingContentCSS = css`
-	width: ${720 / PIXEL_REM_RATIO}rem;
-`;
 
 function returnModals(contents: contentsOfModal) {
 	const { title, host, tags, countries, duration, escalation } = contents;
@@ -180,15 +122,75 @@ function returnModals(contents: contentsOfModal) {
 	);
 }
 
+const monitoringCSS = css`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const buttonCSS = css`
+	background: #5286ff;
+	border-radius: ${4 / PIXEL_REM_RATIO}rem;
+	padding: ${8 / PIXEL_REM_RATIO}rem ${8 / PIXEL_REM_RATIO}rem;
+	min-width: ${180 / PIXEL_REM_RATIO}rem;
+	font-size: ${16 / PIXEL_REM_RATIO}rem;
+	font-weight: 600;
+	font-family: Gilroy;
+	color: #fff;
+	text-align: center;
+`;
+
+const modalCSS = css`
+	font-family: Gilroy;
+	background: #ffffff;
+	border: 2px solid #e6e6e6;
+	box-sizing: border-box;
+	border-radius: 8px;
+	display: flex;
+	flex-direction: column;
+	padding: 1.5rem;
+	margin: 1rem;
+	width: 45rem;
+`;
+
+const titleCSS = css`
+	font-family: Gilroy;
+	font-size: 1.2rem;
+	line-height: 1.25rem;
+	color: #323232;
+	padding: 0.5rem;
+	margin-top: 0rem;
+`;
+
+const keysCSS = css`
+	font-size: 0.9rem;
+	line-height: 1.125rem;
+	color: #9b9b9b;
+	padding: 0.5rem;
+`;
+
+const valuesCSS = css`
+	font-size: 0.9rem;
+	line-height: 1.125rem;
+	color: #323232;
+`;
+
+const settingContentCSS = css`
+	width: ${720 / PIXEL_REM_RATIO}rem;
+`;
+
 const viewBuildCSS = css`
 	background: #ffffff;
 	border: 1px solid #c4c4c4;
 	box-sizing: border-box;
 	border-radius: 6px;
-    font-family: Gilroy;
-    font-weight: 600;
+	font-family: Gilroy;
+	font-weight: 600;
 	font-size: 14px;
 	line-height: 16px;
+	height: 1.75rem;
+	width: 7.5rem;
 	color: #323232;
 `;
 

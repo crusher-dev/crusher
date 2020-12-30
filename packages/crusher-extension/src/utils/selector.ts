c74cbaf55c01d42231bf0021ab2a4615c155dca9
Utkarsh <utkarshdix02@gmail.com>
2020-12-30 08:24:36 +0530

    (Extension): Cleanup background script and util functions and enable typescript

--- packages/crusher-extension/src/utils/selector.ts
+++ packages/crusher-extension/src/utils/selector.ts
@@ -1,43 +1,64 @@
-import { finder } from '@medv/finder';
+import { finder } from "@medv/finder";
+import * as uniqueSelector2 from "unique-selector-2";
 
-const uniqueSelector = require('unique-selector');
-const uniqueSelector2 = require('unique-selector-2');
+const _uniqueSelector2 = new uniqueSelector2.default({});
 
-function getXpathTo(element: any): any {
-	if (element.id !== '') return `id("${element.id}")`;
+function getXpathTo(element: HTMLElement): string | null {
 	if (element === document.body) return element.tagName;
+	if (!element.parentNode) return null;
 
 	let ix = 0;
 	const siblings = element.parentNode.childNodes;
+
 	for (let i = 0; i < siblings.length; i++) {
-		const sibling = siblings[i];
-		if (sibling === element) return `${getXpathTo(element.parentNode)}/${element.tagName}[${ix + 1}]`;
-		if (sibling.nodeType === 1 && sibling.tagName === element.tagName) ix++;
+		const sibling: ChildNode = siblings[i];
+		if (sibling === element) {
+			return `${getXpathTo(element.parentNode as HTMLElement)}/${
+				element.tagName
+			}[${ix + 1}]`;
+		}
+		if (
+			sibling.nodeType === 1 &&
+			(sibling as HTMLElement).tagName === element.tagName
+		)
+			ix++;
 	}
+
+	return null;
 }
 
-function getFinderSelector(elementNode: HTMLElement) {
-	const optimizedMinLength = elementNode.getAttribute('id') ? 2 : 10; // if the target has an id, use that instead of multiple frames selectors
-	// @ts-ignore
+function getFinderSelector(elementNode: HTMLElement): string {
+	const optimizedMinLength = elementNode.getAttribute("id") ? 2 : 10; // if the target has an id, use that instead of multiple frames selectors
 	return finder(elementNode, { seedMinLength: 5, optimizedMinLength });
 }
 
-export function getSelectors(elementNode: HTMLElement) {
-	const _uniqueSelector2 = new uniqueSelector2.default({});
-	const selectors = _uniqueSelector2.getUniqueSelector(elementNode);
+export interface iSelectorInfo {
+	type: string;
+	value: string;
+	uniquenessScore: number;
+}
 
-	return [
-		{
-			type: 'customFinder',
-			value: getFinderSelector(elementNode),
+export function getSelectors(elementNode: HTMLElement): Array<iSelectorInfo> {
+	const selectors = _uniqueSelector2.getUniqueSelector(elementNode);
+	const finderSelector = getFinderSelector(elementNode);
+	const xPathSelector = getXpathTo(elementNode);
+
+	const out = [];
+	if (finderSelector) {
+		out.push({
+			type: "customFinder",
+			value: finderSelector as string,
 			uniquenessScore: 1,
-		},
-		{
-			type: 'uniqueSelector',
-			value: uniqueSelector.default(elementNode),
+		});
+	}
+	if (xPathSelector) {
+		out.push({
+			type: "xpath",
+			value: getXpathTo(elementNode) as string,
 			uniquenessScore: 1,
-		},
-		{ type: 'xpath', value: getXpathTo(elementNode), uniquenessScore: 1 },
-		...selectors.list,
-	];
+		});
+	}
+	out.push(...(selectors.list as Array<iSelectorInfo>));
+
+	return out;
 }

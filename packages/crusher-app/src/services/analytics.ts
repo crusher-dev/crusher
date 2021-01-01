637349601c60b9e45c85d8a746556e4e75ed7512
Himanshu Dixit <hudixt@gmail.com>
2021-01-01 22:23:30 +0530

    Setup basic tracking

--- packages/crusher-app/src/services/analytics.ts
+++ packages/crusher-app/src/services/analytics.ts
@@ -1,11 +1,7 @@
-import jsCookie from "js-cookie";
+import {PAGE_TYPE} from "@constants/page";
 
 class AnalyticsService {
-	private userId: string | null;
-	private teamId: string | null;
-	private projectId: string | null;
-
-	constructor(
+	 intialize(
 		userId: string | null,
 		teamId: string | null,
 		projectId: string | null,
@@ -14,46 +10,26 @@ class AnalyticsService {
 			console.log(
 				"Tracking user with anonymous identity. Make sure to call addUser when user Sign in.",
 			);
-
-		userId = window.analytics.identify();
-		jsCookie.set("userId", userId);
-
-		this.userId = userId;
-		this.teamId = teamId;
-		this.projectId = projectId;
 	}
 
-	trackPage() {}
+	 trackPage(pageType: PAGE_TYPE) {
+		 window.analytics.page(pageType);
+	}
 
-	trackEvent() {}
+	 trackEvent() {}
 
-	trackCustomEvent() {}
+	 trackCustomEvent() {}
 
-	addGroupId() {}
+	 addGroupId() {}
 
-	addGroupTrait() {}
+	 addGroupTrait() {}
 
-	addUserId() {}
+	 addUserId() {}
 
-	addUserTrait() {}
+	 addUserTrait() {}
 }
 
 /*
     Pass userId, teamId, projectId after reading from cookies
  */
-export const initializeAnalytics = (
-	userId: string | null,
-	teamId: string | null,
-	projectId: string | null,
-) => {
-	if (typeof window === "undefined") {
-		console.log("Initialized at server leve");
-		return;
-	}
-	// @ts-ignore
-	Analytics = new AnalyticsService(userId, teamId, projectId);
-};
-
-export let Analytics = null;
-
-export default Analytics;
+export const ANALYTICS = Object.freeze(new AnalyticsService());

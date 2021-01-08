899329c502407df38a731f75b3f6653b2afda9df
Viresh Uberoy <vireshuberoy01@gmail.com>
2021-01-08 13:29:14 +0530

    {shared} added sample config files for firebase config (both server, and client)

---
+++ packages/crusher-shared/config/fire-config-server.sample.js
@@ -0,0 +1,31 @@
+const firebase = require("firebase");
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+	apiKey: '',
+	authDomain: '',
+	databaseURL: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: '',
+	measurementId: '',
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+   firebase.initializeApp(firebaseConfig);
+} else {
+   firebase.app(); // if already initialized, use that one
+}
+
+try {
+	firebase.initializeApp(firebaseConfig);
+} catch (err) {
+	if (!/already exists/.test(err.message)) {
+		console.error('Firebase initialization error', err.stack);
+	}
+}
+
+const fire = firebase;
+module.exports = fire;

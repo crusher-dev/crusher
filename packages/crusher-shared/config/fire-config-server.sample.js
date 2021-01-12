e554012a6d9b33e56a6b7f9f99c55735d7e15d93
Viresh Uberoy <vireshuberoy01@gmail.com>
2021-01-12 17:54:56 +0530

    {shared} changed config

--- packages/crusher-shared/config/fire-config-server.sample.js
+++ packages/crusher-shared/config/fire-config-server.sample.js
@@ -1,4 +1,3 @@
-const firebase = require("firebase");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
@@ -12,20 +11,4 @@ var firebaseConfig = {
 	measurementId: '',
 };
 
-// Initialize Firebase
-if (!firebase.apps.length) {
-   firebase.initializeApp(firebaseConfig);
-} else {
-   firebase.app(); // if already initialized, use that one
-}
-
-try {
-	firebase.initializeApp(firebaseConfig);
-} catch (err) {
-	if (!/already exists/.test(err.message)) {
-		console.error('Firebase initialization error', err.stack);
-	}
-}
-
-const fire = firebase;
-module.exports = fire;
+module.exports = firebaseConfig;
--- packages/crusher-shared/config/fire-config.sample.js
+++ packages/crusher-shared/config/fire-config.sample.js
@@ -1,5 +1,3 @@
-import firebase from 'firebase';
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
@@ -12,20 +10,5 @@ var firebaseConfig = {
 	appId: '',
 	measurementId: '',
 };
-// Initialize Firebase
-if (!firebase.apps.length) {
-   firebase.initializeApp(firebaseConfig);
-} else {
-   firebase.app(); // if already initialized, use that one
-}
 
-try {
-	firebase.initializeApp(firebaseConfig);
-} catch (err) {
-	if (!/already exists/.test(err.message)) {
-		console.error('Firebase initialization error', err.stack);
-	}
-}
-
-const fire = firebase;
-export default fire;
+export default firebaseConfig;

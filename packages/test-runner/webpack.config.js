333c97f2a9dbe5ccd4525d280b8d8c88a851b1a6
Utkarsh <utkarshdix02@gmail.com>
2021-01-14 21:18:05 +0530

    (Runner): Add test runner dockerfile

---
+++ packages/test-runner/webpack.config.js
@@ -0,0 +1,33 @@
+//webpack.config.js
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	mode: "development",
+	devtool: "inline-source-map",
+	target: "node",
+	entry: {
+		index: "./src/app.ts",
+	},
+	output: {
+		libraryTarget: 'commonjs',
+		path: path.resolve(__dirname, './build'),
+		filename: "[name].js" // <--- Will be compiled to this single file
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.FLUENTFFMPEG_COV': false
+		})
+	],
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader"
+			}
+		]
+	}
+};

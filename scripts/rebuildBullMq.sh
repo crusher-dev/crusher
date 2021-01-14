b2eee792be3620b217469adb2e3f06d92bbda9a6
Utkarsh <utkarshdix02@gmail.com>
2021-01-14 11:13:26 +0530

    (BullMq): Add support for offset infra in bullmq

---
+++ scripts/rebuildBullMq.sh
@@ -0,0 +1,9 @@
+BULLMQ_PACKAGE="node_modules/bullmq"
+
+if [ ! -d "$BULLMQ_PACKAGE" ]; then
+  cd $BULLMQ_PACKAGE
+  echo "Starting building bullmq package again"
+  yarn build
+  cd ../../
+  echo "Finished with rebuilding bullmq package"
+fi

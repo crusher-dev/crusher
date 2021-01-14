e747335bac13c85e06c2dcb8d59f761e3ec594a6
Utkarsh <utkarshdix02@gmail.com>
2021-01-14 20:50:53 +0530

    (Server) Create basic webpack config and dockerfile

---
+++ packages/crusher-server/webpack.config.js
@@ -0,0 +1,27 @@
+//webpack.config.js
+const path = require('path');
+
+module.exports = {
+	mode: "development",
+	devtool: "inline-source-map",
+	target: "node",
+	entry: {
+		index: "./src/app.ts",
+	},
+	output: {
+		libraryTarget: 'commonjs',
+		path: path.resolve(__dirname, './build'),
+		filename: "[name].js" // <--- Will be compiled to this single file
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader"
+			}
+		]
+	}
+};

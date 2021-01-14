439084b9e2710765a56aff549a5fea08b9705493
Himanshu Dixit <hudixt@gmail.com>
2021-01-15 03:01:40 +0530

    Add webpack

--- packages/crusher-server/webpack.config.js
+++ packages/crusher-server/webpack.config.js
@@ -1,21 +1,58 @@
+// //webpack.config.js
+// const path = require('path');
+//
+// module.exports = {
+// 	mode: "development",
+// 	devtool: "inline-source-map",
+// 	target: "node",
+// 	entry: {
+// 		index: "./src/app.ts",
+// 	},
+// 	output: {
+// 		libraryTarget: 'commonjs',
+// 		path: path.resolve(__dirname, './build'),
+// 		filename: "[name].js" // <--- Will be compiled to this single file
+// 	},
+// 	resolve: {
+// 		extensions: [".ts", ".tsx", ".js"],
+// 	},
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.tsx?$/,
+// 				loader: "ts-loader"
+// 			}
+// 		]
+// 	}
+// };
+
 //webpack.config.js
 const path = require('path');
 
 module.exports = {
-	mode: "development",
-	devtool: "inline-source-map",
+	mode: 'development',
+	devtool: 'source-map',
 	target: "node",
 	entry: {
 		index: "./src/app.ts",
 	},
 	output: {
-		libraryTarget: 'commonjs',
 		path: path.resolve(__dirname, './build'),
-		filename: "[name].js" // <--- Will be compiled to this single file
+		filename: 'app.js',
+		chunkFilename: `[name]-[chunkhash:4].js`,
+		sourceMapFilename: `[name]-[chunkhash:4].js.map`,
+		libraryTarget: 'commonjs',
 	},
 	resolve: {
 		extensions: [".ts", ".tsx", ".js"],
 	},
+	// optimization: {
+	// 	splitChunks: {
+	// 		chunks: 'initial',
+	// 		minSize: 210000,
+	// 		maxSize: 500000,
+	// 	}
+	// },
 	module: {
 		rules: [
 			{

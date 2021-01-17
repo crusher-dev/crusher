61b01e26a323c97bd2746e0c93f66e45a97bc8e7
Utkarsh <utkarshdix02@gmail.com>
2021-01-17 23:26:28 +0530

    (CI) Update crusher-server workflow

--- ecosystem.config.sample.js
+++ ecosystem.config.sample.js
@@ -19,9 +19,9 @@ const MONGODB_CONFIG = {
 
 const REDIS_CONFIG = {
     CONNECTION_STRING: process.env.REDIS_CONNECTION_STRING ? process.env.REDIS_CONNECTION_STRING : null,
-    HOST: "localhost",
-    PORT: 6379,
-    PASSWORD: ""
+    HOST:  process.env.REDIS_HOST ? process.env.REDIS_HOST : "localhost",
+    PORT:  process.env.REDIS_PORT ? process.env.REDIS_PORT : 6379,
+    PASSWORD: process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : ""
 };
 
 const GOOGLE_CONFIG = {

cc21770d2570c982e73c16f13dc8a2a0a2c121ae
Utkarsh <utkarshdix02@gmail.com>
2021-01-18 02:13:12 +0530

    (Server) Remove sensitive information from logs

--- packages/crusher-server/src/core/manager/MongoManager.ts
+++ packages/crusher-server/src/core/manager/MongoManager.ts
@@ -11,20 +11,20 @@ export default class MongoManager {
 		mongoose.connect(connectionString);
 
 		mongoose.connection.on("connected", function () {
-			console.log("Connected to " + connectionString);
+			console.log("Connected to mongodb failed");
 		});
 
 		mongoose.connection.on("error", function (error) {
-			console.log("Connection to " + connectionString + " failed:" + error);
+			console.log("Connection to mongodb failed:" + error);
 		});
 
 		mongoose.connection.on("disconnected", function () {
-			console.log("Disconnected from " + connectionString);
+			console.log("Disconnected from mongodb");
 		});
 
 		process.on("SIGINT", function () {
 			mongoose.connection.close(function () {
-				console.log("Disconnected from " + connectionString + " through app termination");
+				console.log("Disconnected from mongodb through app termination");
 				process.exit(0);
 			});
 		});

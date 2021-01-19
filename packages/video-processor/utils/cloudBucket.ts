f774f88b06e1780c7cd846c9edd6edbcf29223ee
Utkarsh <utkarshdix02@gmail.com>
2021-01-19 22:25:20 +0530

    (Video Processor) Log error on upload

--- packages/video-processor/utils/cloudBucket.ts
+++ packages/video-processor/utils/cloudBucket.ts
@@ -21,7 +21,7 @@ export const s3BucketService = new AWS.S3({
 export async function uploadFileToAwsBucket(s3Bucket, filePath: string, fileName: string, destination: string = '/') {
 	return new Promise((resolve, reject) => {
 		const fileStream = fs.createReadStream(filePath);
-		fileStream.on('error', function(err) {
+		fileStream.on('error', function (err) {
 			reject({ message: 'File Error', err: err });
 		});
 		console.log(destination, fileName);
@@ -31,12 +31,15 @@ export async function uploadFileToAwsBucket(s3Bucket, filePath: string, fileName
 				Key: destination + '/' + fileName,
 				Body: fileStream,
 			},
-			function(err, data) {
-				if (err) reject({ message: 'File upload failed', err: err });
+			function (err, data) {
+				if (err) {
+					console.error(err);
+					return reject({ message: 'File upload failed', err: err });
+				}
 
 				const url = s3BucketService.getSignedUrl('getObject', {
 					Bucket: VIDEO_BUCKET_NAME,
-					Key: data.key,
+					Key: data.Key,
 					Expires: 60 * 60 * 24 * 6,
 				});
 

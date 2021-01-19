c53e6d83790f1766ea42cfbc5b62b10a327a43e3
Utkarsh <utkarshdix02@gmail.com>
2021-01-19 15:48:58 +0530

    (Runner): Cleanup and refractor test runner

---
+++ packages/test-runner/src/manager/redis.ts
@@ -0,0 +1,14 @@
+import IORedis, { Redis } from 'ioredis';
+
+export class RedisManager{
+	client: Redis;
+
+	constructor(host: string, port: number, password: string) {
+		this.client = new IORedis({ host, port, password });
+		console.log(`Connected to ${host}:${port}`);
+	}
+
+	get() {
+		return this.client;
+	}
+};

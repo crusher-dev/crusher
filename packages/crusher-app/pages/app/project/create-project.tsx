e2c84a4596f411b05a5f145122f6f92974289c6b
Viresh Uberoy <vireshuberoy01@gmail.com>
2021-01-21 18:06:17 +0530

    {app} added progress bar, selected cards style and changed .png (go back) to .svg

--- packages/crusher-app/pages/app/project/create-project.tsx
+++ packages/crusher-app/pages/app/project/create-project.tsx
@@ -1,112 +1,164 @@
 import { css } from "@emotion/core";
-import GoBack from "../../../public/assets/img/general/goback.png";
+import GoBack from "../../../public/svg/project/goBack.svg";
 import TypeOfProject from "@ui/components/project/TypeOfProject";
 import GrayPlaceholder from "../../../public/assets/img/general/grayPlaceholder.png";
+import { useState } from "react";
 // import Input from "@ui/components/form/Input";
 
 function CreateProject() {
-	const testingObject = {
-		title: "Testing",
+	const [isTextFieldSelected, setIsTextFieldSelected] = useState(false);
+	const [textFieldData, setTextFieldData] = useState("");
+	const [selectedCardID, setSelectedCardID] = useState("");
+
+	const sectionData = {
 		cards: [
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "APP Testing",
 				cardDescription: "Android/iOS App",
+				cardID: "appTesting",
 			},
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "Web Testing",
 				cardDescription: "Android/iOS App",
+				cardID: "webTesting",
 			},
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "API Testing",
 				cardDescription: "Android/iOS App",
+				cardID: "apiTesting",
 			},
 		],
 	};
 
 	const monitoringObject = {
-		title: "Monitoring",
 		cards: [
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "External Monitoring",
 				cardDescription: "Android/iOS App",
+				cardID: "externalMonitoring",
 			},
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "APM",
 				cardDescription: "Android/iOS App",
+				cardID: "apm",
 			},
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "Infra",
 				cardDescription: "Android/iOS App",
+				cardID: "infra",
 			},
 		],
 	};
 
 	const otherObject = {
-		title: "Other",
 		cards: [
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "Workflow Automation",
 				cardDescription: "Android/iOS App",
+				cardID: "workflowAutomation",
 			},
 			{
 				cardIllustration: GrayPlaceholder,
 				cardName: "Testing",
 				cardDescription: "Android/iOS App",
-            },
+				cardID: "testing",
+			},
 		],
 	};
 
+	function handleCardClick(cardID: any) {
+		setSelectedCardID(cardID);
+	}
+
 	return (
-		<div css={containerCSS}>
-			<div css={goBackCSS}>
-				<img
-					src={GoBack}
-					css={css`
-						height: 1rem;
-						padding-right: 0.5rem;
-					`}
+		<>
+			<div
+				css={[
+					progressBarCSS,
+					textFieldData || selectedCardID ? progressBarFirstStepDoneCSS : null,
+					selectedCardID && textFieldData ? progressBarSecondStepDoneCSS : null,
+				]}
+			></div>
+			<div css={containerCSS}>
+				<div css={goBackCSS}>
+					<GoBack css={goBackSVGCSS} />
+					<span>Go Back</span>
+				</div>
+				<div css={titleCSS}>
+					<p>What is the name of the project?</p>
+					<input
+						css={[textFieldCSS, isTextFieldSelected ? selectedTextFieldCSS : null]}
+						type="text"
+						onFocus={() => {
+							setIsTextFieldSelected(true);
+						}}
+						onChange={(e) => setTextFieldData(e.target.value)}
+					/>{" "}
+					{/* Should I use Input Component in @ui/componentes/form/Input? */}
+				</div>
+				<div css={titleCSS}>Select type of project</div>
+				<TypeOfProject
+					{...sectionData}
+					title="Testing"
+					onSelect={handleCardClick}
+					selectedID={selectedCardID}
 				/>
-				<span>Go Back</span>
+				<TypeOfProject
+					{...monitoringObject}
+					title="Monitoring"
+					onSelect={handleCardClick}
+					selectedID={selectedCardID}
+				/>
+				<TypeOfProject
+					{...otherObject}
+					title="Other"
+					onSelect={handleCardClick}
+					selectedID={selectedCardID}
+				/>
+				<button
+					css={[
+						buttonCSS,
+						textFieldData && selectedCardID ? fulfilledButtonCSS : null,
+					]}
+				>
+					Create Project
+				</button>
 			</div>
-			<div css={titleCSS}>
-				<p>What is the name of the project?</p>
-				<input css={textFieldCSS} type="text" />{" "}
-				{/* Should I use Input Component in @ui/componentes/form/Input? */}
-			</div>
-			<div css={titleCSS}>Select type of project</div>
-			<TypeOfProject {...testingObject} />
-			<TypeOfProject {...monitoringObject} />
-            <TypeOfProject {...otherObject} />
-            <button css={buttonCSS}>Create Project</button>
-		</div>
+		</>
 	);
 }
 
 const buttonCSS = css`
-    width: 16rem;
-    font-size: 1rem;
-    line-height: 20px;
-    height: 2.5rem;
-    background: #DDDDDD;
-    border-radius: 0.5rem;
-    color: #FFFFFF;
-    font-weight: 600;
-    position: absolute;
-    right: 5rem;
-    bottom: 5rem;
-`
+	width: 16rem;
+	font-size: 1rem;
+	line-height: 20px;
+	height: 2.5rem;
+	background: #dddddd;
+	border-radius: 0.5rem;
+	color: #ffffff;
+	font-weight: 600;
+	position: absolute;
+	right: 5rem;
+	bottom: 5rem;
+`;
+
+const fulfilledButtonCSS = css`
+	background: #6583fe;
+	border: 2px solid #5173fd;
+	border-radius: 4px;
+`;
 
 const containerCSS = css`
-    margin: 1.5rem;
-    height: 100vh;
-    width: 100vw;
+	margin: 1.5rem;
+	height: 100vh;
+	width: 100vw;
 `;
 
 const goBackCSS = css`
@@ -117,6 +169,12 @@ const goBackCSS = css`
 	font-size: 1rem;
 	font-family: Gilroy;
 	margin-bottom: 2rem;
+`;
+
+const goBackSVGCSS = css`
+	height: 0.75rem;
+	width: 1rem;
+	margin-right: 0.5rem;
 `;
 
 const titleCSS = css`
@@ -131,10 +189,33 @@ const textFieldCSS = css`
 	margin-bottom: 2.5rem;
 	border: 1px solid #c4c4c4;
 	box-sizing: border-box;
-    border-radius: 4px;
-    padding: 0.5rem;
-    padding-left: 0.5rem;
-    font-size: 1.5rem;
+	border-radius: 4px;
+	padding: 0.5rem;
+	padding-left: 0.5rem;
+	font-size: 1rem;
+	line-height: 1rem;
+	width: 20rem;
+`;
+
+const selectedTextFieldCSS = css`
+	background: #ffffff;
+	border: 2px solid #6583fe;
+	border-radius: 4px;
+`;
+
+const progressBarCSS = css`
+	width: 5%;
+	background: #6583fe;
+	transition: width 1s ease-in-out;
+	height: 0.5rem;
+`;
+
+const progressBarFirstStepDoneCSS = css`
+	width: 50%;
+`;
+
+const progressBarSecondStepDoneCSS = css`
+	width: 100%;
 `;
 
 export default CreateProject;
--- packages/crusher-app/src/ui/components/project/TypeOfProject.tsx
+++ packages/crusher-app/src/ui/components/project/TypeOfProject.tsx
@@ -1,39 +1,52 @@
 import { css } from "@emotion/core";
 
 export default function TypeOfProject(props: any) {
-	const { title, cards } = props;
+	const { title, cards, onSelect, selectedID } = props;
+
+	function handleOnClick(e: any) {
+		const { id } = e.target;
+		onSelect(id);
+	}
+
 	return (
 		<div css={sectionCSS}>
 			<p css={titleCSS}>{title}</p>
-			<div
-				css={css`
-                    display: flex;
-                    flex-wrap: wrap;
-                    max-width: 75%;
-				`}
-			>
-				{cards.map((card: any) => returnCard({ ...card }))}
+			<div css={cardContainerCSS}>
+				{cards.map((card: any) => returnCard({ ...card }, selectedID))}
 			</div>
 		</div>
 	);
-}
 
-function returnCard({ cardIllustration, cardName, cardDescription }) {
-	return (
-		<div css={cardCSS}>
-			<img src={cardIllustration} css={imageCSS} />
-			<div css={cardTextCSS}>
-				<strong>{cardName}</strong>
-				<p>{cardDescription}</p>
+	function returnCard(
+		{ cardIllustration, cardName, cardDescription, cardID },
+		selectedID,
+	) {
+		return (
+			<div
+				css={[cardCSS, cardID === selectedID ? selectedProjectTypeCSS : null]}
+				id={cardID}
+				onClick={(e) => handleOnClick(e)}
+			>
+				<img src={cardIllustration} css={imageCSS} id={cardID} />
+				<div css={cardTextCSS} id={cardID}>
+					<strong id={cardID}>{cardName}</strong>
+					<p id={cardID}>{cardDescription}</p>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 const sectionCSS = css`
 	display: flex;
-    flex-direction: column;
-    margin-bottom: 2rem;
+	flex-direction: column;
+	margin-bottom: 2rem;
+`;
+
+const cardContainerCSS = css`
+	display: flex;
+	flex-wrap: wrap;
+	max-width: 75%;
 `;
 
 const titleCSS = css`
@@ -58,14 +71,24 @@ const cardTextCSS = css`
 `;
 
 const cardCSS = css`
+	z-index: 11;
 	display: flex;
 	align-items: center;
 	height: 4rem;
-    width: 17rem;
+	width: 17rem;
 	border: 1px solid #c4c4c4;
 	box-sizing: border-box;
 	border-radius: 4px;
 	padding: 1rem;
-    margin-top: 0.5rem;
-    margin-right: 2.5rem;
+	margin-top: 0.5rem;
+	margin-right: 2.5rem;
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+const selectedProjectTypeCSS = css`
+	background: rgba(101, 131, 254, 0.1);
+	border: 2px solid rgba(101, 131, 254, 0.9);
+	border-radius: 4px;
 `;

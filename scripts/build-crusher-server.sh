e0bb02be00729eb37187e2dd226ff06aca618040
Utkarsh <utkarshdix02@gmail.com>
2021-02-04 18:36:02 +0530

    (Server) Update build crusher server script

--- scripts/build-crusher-server.sh
+++ scripts/build-crusher-server.sh
@@ -1,1 +1,1 @@
-cp -R ./node_modules/bullmq ./packages/crusher-server/bullmq && cd packages/crusher-server && yarn build && cd ../../
+cp -R ./node_modules/bullmq ./packages/crusher-server/bullmq && cd packages/crusher-server && yarn build && yarn build:cron && cd ../../

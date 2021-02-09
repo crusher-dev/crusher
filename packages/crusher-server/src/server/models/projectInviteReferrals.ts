600d8c3a20cfe48c0d2ebc4120190288dde30db6
Utkarsh <utkarshdix02@gmail.com>
2021-02-10 01:47:39 +0530

    (App, Server) Show public invite link url for projects

--- packages/crusher-server/src/server/models/projectInviteReferrals.ts
+++ packages/crusher-server/src/server/models/projectInviteReferrals.ts
@@ -2,10 +2,11 @@ import * as mongoose from "mongoose";
 
 const ProjectInviteReferralsSchema = new mongoose.Schema(
 	{
-		teamId: {type: mongoose.Schema.Types.Number, required: true},
-		projectId: {type: mongoose.Schema.Types.Number, required: true},
-		expiresOn: {type: mongoose.Schema.Types.Date, required: false},
-		meta: {type: mongoose.Schema.Types.Mixed, required: false}
+		teamId: { type: mongoose.Schema.Types.Number, required: true },
+		projectId: { type: mongoose.Schema.Types.Number, required: true },
+		expiresOn: { type: mongoose.Schema.Types.Date, required: false },
+		meta: { type: mongoose.Schema.Types.Mixed, required: false },
+		isPublic: { type: mongoose.Schema.Types.Boolean, required: false },
 	},
 	{ timestamps: true },
 );

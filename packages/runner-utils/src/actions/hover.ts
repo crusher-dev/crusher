bf9d911576fc6b8d863bf11976dacf38450a5077
Utkarsh Dixit <utkarshdix02@gmail.com>
2021-02-13 12:00:31 +0530

    (Test Runner) Add crusher multiple selector engine (#79)
    * (Test Runner, Code Generator, Runner Utils): Add support for multiple selector engine to improve performance
    
    * (Runner Utils) Delete base64 util

--- packages/runner-utils/src/actions/hover.ts
+++ packages/runner-utils/src/actions/hover.ts
@@ -1,23 +1,19 @@
-import { waitForSelectors } from "../functions";
 import { Page } from "playwright";
 import { iAction } from "@crusher-shared/types/action";
 import { iSelectorInfo } from "@crusher-shared/types/selectorInfo";
+import { toCrusherSelectorsFormat } from '../utils/helper';
+import { waitForSelectors } from '../functions';
 
 export default async function hover(action: iAction, page: Page) {
 	return new Promise(async (success, error) => {
 		try {
 			const selectors = action.payload.selectors as iSelectorInfo[];
-			const selector = await waitForSelectors(page, selectors);
 
-
-			if(!selector || typeof selector !== "string"){
-				return error(`Invalid selector`);
-			}
-
-			await page.hover(selector);
+			await waitForSelectors(page, selectors);
+			await page.hover(toCrusherSelectorsFormat(selectors));
 
 			return success({
-				message: `Hovered on the element ${selector}`,
+				message: `Hovered on the element ${selectors[0].value}`,
 			});
 		} catch(err){
 			return error("Some issue occurred while hovering on element");
--- packages/runner-utils/src/functions/scroll.ts
+++ packages/runner-utils/src/functions/scroll.ts
@@ -1,12 +1,69 @@
 import { Page } from "playwright";
+import { iSelectorInfo } from '@crusher-shared/types/selectorInfo';
 
 export default async function scroll(
 	page: Page,
-	selector: string,
+	selectors: Array<iSelectorInfo>,
 	scrollDeltaArr: Array<number>,
+	isWindow: boolean = true
 ) {
 	await page.evaluate(
-		 ([scrollDeltaArr, selectorKey]: [number[], string]) => {
+		 ([scrollDeltaArr, selectorKeys, isWindow]: [number[], Array<iSelectorInfo>, boolean]) => {
+			 const getElementsByXPath = (
+				 xpath: string,
+				 parent: Node | null = null,
+			 ): Node[] => {
+				 const results = [];
+				 const query = document.evaluate(
+					 xpath,
+					 parent || document,
+					 null,
+					 XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+					 null,
+				 );
+				 for (let i = 0, length = query.snapshotLength; i < length; ++i) {
+					 const item = query.snapshotItem(i);
+					 if (item) results.push(item);
+				 }
+				 return results;
+			 };
+
+			 const generateQuerySelector = (el: HTMLElement): string => {
+				 if (el.tagName.toLowerCase() == "html") return "HTML";
+				 let str = el.tagName;
+				 str += el.id != "" ? "#" + el.id : "";
+				 if (el.className) {
+					 const classes = el.className.split(/\s/);
+					 for (let i = 0; i < classes.length; i++) {
+						 str += "." + classes[i];
+					 }
+				 }
+				 return generateQuerySelector((el as any).parentNode) + " > " + str;
+			 };
+
+			 const getValidSelectorFromArr = (selectors: Array<iSelectorInfo>, root: Element | Document = document) => {
+				 for (const selector of selectors) {
+					 try {
+						 if (selector.type === "xpath") {
+							 const elements = getElementsByXPath(selector.value);
+							 if (elements.length) {
+								 const elementSelectorFromXpath = generateQuerySelector(
+									 elements[0] as HTMLElement,
+								 );
+
+								 return {element: elements[0] as Element, selector: elementSelectorFromXpath};
+							 }
+						 } else if (root.querySelector(selector.value)) {
+							 return {element: root.querySelector(selector.value)!, selector: selector.value};
+						 }
+					 } catch{}
+				 }
+				 return null;
+			 };
+
+		 	const selectorKeyInfo = getValidSelectorFromArr(selectorKeys);
+		 	if(!selectorKeyInfo && !isWindow) throw new Error("No valid selector found");
+
 			const scrollTo =  function (element: HTMLElement, offset: number) {
 				const fixedOffset = offset.toFixed();
 				const onScroll = () => {
@@ -25,13 +82,12 @@ export default async function scroll(
 				});
 			};
 
-			const element =
-				selectorKey === "window" ? window : document.querySelector(selectorKey);
+			const element = isWindow? window : document.querySelector(selectorKeyInfo!.selector);
 
 			for (let i = 0; i < scrollDeltaArr.length; i++) {
 				 scrollTo(element as HTMLElement, scrollDeltaArr[i]);
 			}
 		},
-		[scrollDeltaArr, selector],
+		[scrollDeltaArr, selectors, isWindow],
 	);
 }

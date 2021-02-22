d88740b8c12728541882555b09a61d923e56b721
Utkarsh <utkarshdix02@gmail.com>
2021-02-22 17:19:45 +0530

    (Extension) Change build mode to production from development

--- packages/crusher-extension/webpack/webpack.ts
+++ packages/crusher-extension/webpack/webpack.ts
@@ -5,7 +5,7 @@ import * as webpack from "webpack";
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-	mode: "development",
+	mode: "production",
 	entry: {
 		content_script: [
 			path.resolve(__dirname, "../src/scripts/inject/events_listener.ts"),

f6ae4e4196d6e0770b243bb875c38ca9edf1a692
Utkarsh Dixit <utkarshdix02@gmail.com>
2021-02-26 16:53:44 +0530

    (Extension) Add base onboarding (#91)
    * (Extension) Add onboarding
    
    * (Extension) Remove onboarding step for seo assertion
    
    * (Extension) Disable onboarding

--- packages/crusher-shared/utils/extension.ts
+++ packages/crusher-shared/utils/extension.ts
@@ -3,13 +3,17 @@ import * as path from 'path';
 import { DEVICE_TYPES } from '../types/deviceTypes';
 import { iDevice } from '../types/extension/device';
 
-const generateCrusherExtensionUrl = (baseUrl: string, targetSiteUrl: string, selectedDevice: string): string => {
+const generateCrusherExtensionUrl = (baseUrl: string, targetSiteUrl: string, selectedDevice: string, queries: any = {}): string => {
 	const url = new URL(targetSiteUrl);
 
 	const crusherAgent = devices.find((device) => device.id === selectedDevice);
 	url.searchParams.set('__crusherAgent__', encodeURI(crusherAgent!.userAgent));
 
-	return `${path.join(baseUrl, 'test_recorder.html')}?url=${url}&device=${selectedDevice}`;
+	const queryString: string = Object.keys(queries).reduce((prev: string, queryParam: string, index: number) => {
+		return `&${queryParam}=${queries[queryParam]}`
+	}, "");
+
+	return `${path.join(baseUrl, 'test_recorder.html')}?url=${url}&device=${selectedDevice}` + queryString;
 };
 
 const getDefaultDeviceFromDeviceType = (type: DEVICE_TYPES): iDevice | null => {

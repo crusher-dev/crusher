806732e54678219333018545f282061bbf99a11e
Himanshu Dixit <hudixt@gmail.com>
2021-05-30 09:32:30 +0530

    (crushe-electron-app): removde build from tracking

--- packages/crusher-electron-app/build/preload.js
+++
@@ -1,6 +0,0 @@
-var _a = require('electron'), contextBridge = _a.contextBridge, ipcRenderer = _a.ipcRenderer;
-contextBridge.exposeInMainWorld('electron', {
-    turnOnInspectMode: function () { return ipcRenderer.send('turn-on-inspect-mode'); },
-    turnOffInspectMode: function () { return ipcRenderer.send('turn-off-inspect-mode'); }
-});
-//# sourceMappingURL=preload.js.map

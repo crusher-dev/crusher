f6e0acd08dbbe5b16efb81f423fa124707f31ab3
Utkarsh <utkarshdix01@gmail.com>
2021-06-05 12:42:28 +0530

    (App) Add edit icon to edit test name and set execution time accuracy in review page to one digit

---
+++ packages/crusher-app/src/utils/dom.ts
@@ -0,0 +1,55 @@
+function createRange(
+	node: Node,
+	chars: { count: number },
+	range: Range | null = null,
+): Range {
+	if (!range) {
+		range = document.createRange();
+		range.selectNode(node);
+		range.setStart(node, 0);
+	}
+
+	if (chars.count === 0) {
+		range.setEnd(node, chars.count);
+	} else if (node && chars.count > 0) {
+		if (node.nodeType === Node.TEXT_NODE) {
+			if (node.textContent!.length < chars.count) {
+				chars.count -= node.textContent!.length;
+			} else {
+				range.setEnd(node, chars.count);
+				chars.count = 0;
+			}
+		} else {
+			for (let lp = 0; lp < node.childNodes.length; lp++) {
+				range = createRange(node.childNodes[lp], chars, range);
+
+				if (chars.count === 0) {
+					break;
+				}
+			}
+		}
+	}
+
+	return range;
+}
+
+function setCurrentCursorPositionInContentEditable(
+	root: HTMLElement,
+	chars: number,
+) {
+	if (chars >= 0) {
+		const selection = window.getSelection();
+
+		const range = createRange(root.parentNode!, {
+			count: chars,
+		});
+
+		if (range) {
+			range.collapse(false);
+			selection!.removeAllRanges();
+			selection!.addRange(range);
+		}
+	}
+}
+
+export { setCurrentCursorPositionInContentEditable };

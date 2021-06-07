a99a3e4235e9ec58d8028f56714ce1b2afe4ed76
Utkarsh <utkarshdix01@gmail.com>
2021-06-07 06:22:17 +0530

    (Server) Add methods to check mysql and mongodb connections are alive

--- packages/crusher-server/src/core/manager/MongoManager.ts
+++ packages/crusher-server/src/core/manager/MongoManager.ts
@@ -1,32 +1,44 @@
 import { getMongoDBConnectionString } from "../../../config/database";
 
 export default class MongoManager {
-	constructor() {}
+	isConnected: boolean;
+
+	constructor() {
+		this.isConnected = false;
+	}
 
 	init() {
 		const mongoose = require("mongoose");
-
 		const connectionString = getMongoDBConnectionString();
-		console.log("This is connection string", connectionString);
+
+		const _this = this;
 		mongoose.connect(connectionString);
 
 		mongoose.connection.on("connected", function () {
+			_this.isConnected = true;
 			console.log("Connected to mongodb successfully");
 		});
 
 		mongoose.connection.on("error", function (error) {
+			_this.isConnected = false;
 			console.log("Connection to mongodb failed:" + error);
 		});
 
 		mongoose.connection.on("disconnected", function () {
+			_this.isConnected = false;
 			console.log("Disconnected from mongodb");
 		});
 
 		process.on("SIGINT", function () {
 			mongoose.connection.close(function () {
+				_this.isConnected = false;
 				console.log("Disconnected from mongodb through app termination");
 				process.exit(0);
 			});
 		});
+	}
+
+	isAlive() {
+		return this.isConnected;
 	}
 }

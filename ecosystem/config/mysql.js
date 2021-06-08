733580bbba2dcd6f014d2ce5b78c491a118b07a0
Himanshu Dixit <hudixt@gmail.com>
2021-06-08 15:15:06 +0530

    Remove redundant tsconfig

--- ecosystem/config/mysql.js
+++ ecosystem/config/mysql.js
@@ -1,6 +1,6 @@
 const MYSQL_DB_CONFIG = {
 	HOST: process.env.DB_HOST ? process.env.DB_HOST : 'localhost',
-	USERNAME: process.env.DB_USERNAME ? process.env.DB_USERNAME : 'remote',
+	USERNAME: process.env.DB_USERNAME ? process.env.DB_USERNAME : 'root',
 	PASSWORD: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : 'password',
 	PORT: process.env.DB_PORT ? process.env.DB_PORT : 3306,
 };

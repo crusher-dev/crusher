c3584dd553946247863bc5098eed7a354bfb513f
Himanshu Dixit <hudixt@gmail.com>
2021-06-12 16:44:21 +0530

    (App): Fix seggergation method and add type

--- packages/crusher-app/src/utils/helpers.ts
+++ packages/crusher-app/src/utils/helpers.ts
@@ -1,7 +1,7 @@
 import { getUserCLIToken } from "@services/cli";
 
-export function isEnterpriseEdition() {
-	return process.env.CRUSHER_MODE === "ee";
+export function getEdition() {
+	return process.env.CRUSHER_MODE
 }
 
 export function findArrayItemByProperty(arr: Array<any>, property: string) {
---
+++ packages/crusher-shared/types/common/general.ts
@@ -0,0 +1,4 @@
+export enum EDITION_TYPE {
+	EE= "ee",
+	OPEN_SOURCE = "open_source"
+}

a7aa4ac6108f76a243eb22d18d1e24650fe32974
Utkarsh <utkarshdix01@gmail.com>
2021-06-20 16:56:54 +0530

    (Crusher) Add setup script

--- setup/index.js
+++ setup/index.js
@@ -1,10 +1,59 @@
-const { Input } = require('enquirer');
+const { Select } = require('enquirer');
+const yargs = require('yargs');
 
-const prompt = new Input({
-	name: 'username',
-	message: 'What is your username?'
-});
+const installOptions = {};
+const modeChoices = ["open-source", "enterprise"];
 
-prompt.run()
-	.then(answer => console.log('Username:', answer))
-	.catch(console.error);
+function preParseArgv() {
+	const argv = yargs.options({
+		m: {
+			alias: "mode",
+			describe: "Version of crusher to be used",
+			choices: modeChoices
+		},
+		bdomain: {
+			alias: "backend-domain",
+			describe: "Backend domain for server request",
+			string: true,
+			default: "localhost:8000"
+		},
+		fdomain: {
+			alias: "frontend-domain",
+			describe: "Frontend domain for app dashboard",
+			string: true,
+			default: "localhost:3000"
+		}
+	})
+		.help()
+		.alias('help', 'h')
+		.argv;
+
+	if(argv.mode) {
+		installOptions["mode"] = argv.mode;
+	}
+	if(argv.bdomain) {
+		installOptions["bdomain"] = argv.bdomain;
+	}
+	if(argv.fdomain) {
+		installOptions["fdomain"] = argv.fdomain;
+	}
+}
+
+async function init() {
+	preParseArgv();
+
+	if(!installOptions["mode"]) {
+		const modePrompt = new Select({
+			name: 'mode',
+			message: 'Which version of crusher do you want to use?',
+			choices: modeChoices
+		});
+
+		installOptions["mode"] = await modePrompt.run();
+	}
+
+	process.env.CRUSHER_MODE = installOptions.mode;
+	require("packages/crusher-app/setup/tsconfig");
+}
+
+init();

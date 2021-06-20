3c5a0605fa4943bb5039aa45b9e0dee53907e6ea
Utkarsh <utkarshdix01@gmail.com>
2021-06-20 19:40:06 +0530

    (Video Processor) Add setup script and open source seggregation

---
+++ packages/video-processor/setup/tsconfig.js
@@ -0,0 +1,52 @@
+const shell = require("shelljs");
+const path = require("path");
+
+require("dotenv").config();
+
+const CRUSHER_MODE = process.env.CRUSHER_MODE ? process.env.CRUSHER_MODE : "open-source";
+
+function getTSConfig() {
+	try {
+		return require("../tsconfig.json");
+	} catch (ex) {
+		return null;
+	}
+}
+
+function handleEESetup() {
+	const currentTSConfig = getTSConfig();
+
+	if (currentTSConfig && currentTSConfig.isEnterprise == "true") {
+		return;
+	}
+
+	shell.rm("-rf", "../tsconfig.json");
+	shell.rm("-rf", path.resolve(__dirname, "../.env"));
+
+	shell.cp(path.resolve(__dirname, ".tsconfig.ee.json.template"), path.resolve(__dirname, "../tsconfig.json"));
+	shell.cp(path.resolve(__dirname, ".env.ee.template"), path.resolve(__dirname, "../.env"));
+}
+
+function handleOpenSourceSetup() {
+	const currentTSConfig = getTSConfig();
+
+	if (currentTSConfig && currentTSConfig.isEnterprise != "true") {
+		return;
+	}
+
+	shell.rm("-rf", path.resolve(__dirname, "../tsconfig.json"));
+	shell.rm("-rf", path.resolve(__dirname, "../.env"));
+
+	shell.cp(path.resolve(__dirname, ".tsconfig.json.template"), path.resolve(__dirname, "../tsconfig.json"));
+	shell.cp(path.resolve(__dirname, ".env.template"), path.resolve(__dirname, "../.env"));
+}
+
+function init() {
+	if (CRUSHER_MODE === "enterprise") {
+		handleEESetup();
+	} else {
+		handleOpenSourceSetup();
+	}
+}
+
+init();
--- packages/video-processor/src/manager/QueueManager.ts
+++ packages/video-processor/src/manager/QueueManager.ts
@@ -1,7 +1,7 @@
 import { Worker, Queue, QueueScheduler } from "bullmq";
 import { REDDIS } from "../../config/database";
 const path = require("path");
-const r = require("../services/videoProcessorWorker");
+const r = require("@services/videoProcessorWorker");
 
 const queue = new Queue("video-processing-queue", { connection: REDDIS });
 
@@ -14,6 +14,6 @@ queue.client.then(async (redisClient) => {
 	await queueScheduler.waitUntilReady();
 
 	new Worker("video-processing-queue", path.resolve("src/services/videoProcessorWorker.ts"), {
-		connection: redisClient
+		connection: redisClient,
 	});
 });

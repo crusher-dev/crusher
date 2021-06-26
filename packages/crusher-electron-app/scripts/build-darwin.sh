dac81919870d68399cb10ad3067389adea80ffd2
Utkarsh Dixit <utkarsh@crusher.dev>
2021-06-27 02:53:29 +0530

    (Electron App) Store symlinks as symlinks when generating zip for releases

--- packages/crusher-electron-app/scripts/build-darwin.sh
+++ packages/crusher-electron-app/scripts/build-darwin.sh
@@ -10,4 +10,4 @@ cp -rfT "$ELECTRON_BUILD" $DIST_DIR/Electron.app/Contents/Resources/app
 
 cd $DIST_DIR
 # Build distribution
-zip -r ../crusher-darwin.zip ./
+zip  --symlinks -r ../crusher-darwin.zip ./

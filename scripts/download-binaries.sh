3794834937294c20487049d91682a55272cdb7c4
Utkarsh <utkarshdix01@gmail.com>
2021-06-27 00:31:15 +0530

    (Electron App) Don't throw error if binary deps already downloaded

--- scripts/download-binaries.sh
+++ scripts/download-binaries.sh
@@ -1,8 +1,9 @@
 ELECTRON_BIN_DIR="packages/crusher-electron-app/bin"
 if [ -d "$ELECTRON_BIN_DIR/linux" ] && [ -d "$ELECTRON_BIN_DIR/darwin" ]; then
-   echo 'Binaries already extracted. Skipping downloading...' >&2
-   exit 1
+   echo 'Binaries already extracted. Skipping downloading...'
+   exit 0
 fi
 
+echo 'Downloading latest binaries...'
 curl -s https://api.github.com/repos/crusherdev/electron/releases/latest  | grep "celectron.*.zip" | cut -d : -f 2,3 | tr -d \" | wget -qi - -cP packages/crusher-electron-app/bin
 yarn workspace crusher-electron-app extract:deps

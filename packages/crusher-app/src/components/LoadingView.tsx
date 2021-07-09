c9a89a8de0485050540d3789afa51ca2014578b0
hardik <web.hardikhere@gmail.com>
2021-07-09 22:34:33 +0530

    (crusher-app) typo fix

--- packages/crusher-app/src/components/LoadingView.tsx
+++ packages/crusher-app/src/components/LoadingView.tsx
@@ -5,7 +5,7 @@ export const LoadingView = () => {
     return <BlankBase>
         <CenterLayout>
             <div>
-                <Logo ImgEelement={<CrusherLogo />} onlyIcon={true} height={"small"} />
+                <Logo imgEelement={<CrusherLogo />} onlyIcon={true} height={"small"} />
                 Firing up all the booster
             </div>
         </CenterLayout>

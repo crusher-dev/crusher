c373b004fb4141a5c62f0bf8343cb7b93c1e27e6
Himanshu Dixit <hudixt@gmail.com>
2021-07-09 15:41:00 +0530

    (Dyson): Savepoint

--- packages/crusher-app/pages/_app.tsx
+++ packages/crusher-app/pages/_app.tsx
@@ -3,24 +3,16 @@ import dynamic from "next/dynamic";
 import { getMetaFromReq } from "@utils/cookies";
 import { NextApiRequest } from "next";
 import { getThemeFromCookieOrReq } from "@utils/styleUtils";
-import { ToastDialog } from "@ui/atom/toastDialog";
 import { AppContext, AppProps } from "next/app";
 
 import "../src/tailwind.css";
 
-const TopProgressBar = dynamic(
-	function () {
-		return import("@ui/components/app/TopProgressBar");
-	},
-	{ ssr: false },
-);
 
 
 function App({ Component, pageProps }: AppProps<any>) {
 	return (
 		<>
-			<TopProgressBar />
-			<ToastDialog />
+
 			<Component {...pageProps} />
 		</>
 	);
--- packages/crusher-app/server.js
+++ packages/crusher-app/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const next = require("next");
 const proxy = require("express-http-proxy");
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 3001;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();

19b4029a044ee9ac94b595951dc788ea854932ce
Utkarsh Dixit <utkarshdix02@gmail.com>
2021-08-08 15:36:33 +0000

    .

--- setup/dbMigration.js
+++ setup/dbMigration.js
@@ -14,7 +14,7 @@ var connectionObject = connectionString
 			port: process.env.DB_PORT,
 			user: process.env.DB_USERNAME,
 			password: process.env.DB_PASSWORD,
-			database: process.env.DB_DATABASE || 'crusher',
+			database: 'crusher2',
 			insecureAuth: true,
 	  };
 var connection = mysql.createConnection(

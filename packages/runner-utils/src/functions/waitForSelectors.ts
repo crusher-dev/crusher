da68b522a9a3bb1d2353f61d5488449fae9c5835
Utkarsh <utkarsh@crusher.dev>
2021-08-09 10:13:54 +0530

    .

--- packages/runner-utils/src/functions/waitForSelectors.ts
+++ packages/runner-utils/src/functions/waitForSelectors.ts
@@ -4,19 +4,19 @@ import { toCrusherSelectorsFormat } from "../utils/helper";
 import { SELECTOR_TYPE } from "../../../unique-selector/src/constants";
 
 export default async function waitForSelectors(page: Page, selectors: Array<iSelectorInfo>): Promise<iSelectorInfo | undefined> {
-	// let playwrightOut: string | null = null;
-	// if (selectors[0].type == SELECTOR_TYPE.PLAYWRIGHT) {
-	// 	try {
-	// 		await page.waitForSelector(selectors[0].value, { state: "attached" });
-	// 		playwrightOut = selectors[0].value;
-	// 	} catch (ex) {}
-	// 	selectors.shift();
-	// }
+	let playwrightOut: string | null = null;
+	if (selectors[0].type == SELECTOR_TYPE.PLAYWRIGHT) {
+		try {
+			await page.waitForSelector(selectors[0].value, { state: "attached" });
+			playwrightOut = selectors[0].value;
+		} catch (ex) {}
+		selectors.shift();
+	}
 
-	// if (playwrightOut) {
-	// 	return {value: playwrightOut as any, type: SELECTOR_TYPE.PLAYWRIGHT};
-	// }
+	if (playwrightOut) {
+		return {value: playwrightOut as any, type: SELECTOR_TYPE.PLAYWRIGHT};
+	}
 	const encodedSelector = toCrusherSelectorsFormat(selectors);
-	const elementHandle = await (await page.waitForSelector(encodedSelector.value)).evaluate(`window["${encodedSelector.uuid}"]`);
+	const elementHandle = await (await page.waitForSelector(encodedSelector.value)).evaluate(`const l = window["${encodedSelector.uuid}"]; delete window["${encodedSelector.uuid}"];  l;`);
 	return {value: (elementHandle as any).selector as any, type: (elementHandle as any).selectorType as any};
 }

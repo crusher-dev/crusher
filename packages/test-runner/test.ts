3ebe92a1b7ed02408ae3b6dbebdd087b5ef774dd
Utkarsh <utkarsh@crusher.dev>
2021-08-13 23:24:00 +0530

    .

--- packages/test-runner/test.ts
+++ packages/test-runner/test.ts
@@ -6,6 +6,7 @@ import { BrowserEnum } from "../crusher-shared/types/browser";
 const codeGenerator = new CodeGenerator({
 	shouldRecordVideo: true,
 	usePlaywrightChromium: false,
+	videoSavePath: "/tmp/crusher-videos/somedir",
 	browser: BrowserEnum.CHROME,
 	assetsDir: `/tmp/crusher/somedir/`,
 });
@@ -108,7 +109,7 @@ const events = [
 
 codeGenerator.getCode(events as any).then((jsCode: string) => {
 	fs.writeFileSync(
-		"out.js",
+		"out.ts",
 		beautify(
 			`(async function() {\n` +
 				`class GlobalManagerPolyfill { map; constructor(){this.map = new Map();} has(key) {return this.map.has(key); } get(key) { return this.map.get(key); } set(key,value){this.map.set(key,value);} } class LogManagerPolyfill { logStep(...args) { console.log(args[2]); }} class StorageManagerPolyfill { uploadBuffer(buffer, destionation) { return "uploadBuffer.jpg"; } upload(filePath, destination) { return "upload.jpg"; } remove(filePath) { return "remove.jpg"; }} const logManager = new LogManagerPolyfill(); const storageManager = new StorageManagerPolyfill(); const globalManager = new GlobalManagerPolyfill();` +

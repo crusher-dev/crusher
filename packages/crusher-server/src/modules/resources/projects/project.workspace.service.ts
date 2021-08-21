4906ae5691948d8d19481774aecf40910928384c
Utkarsh <utkarsh@crusher.dev>
2021-08-21 19:50:04 +0530

    (Server) Delete project workspace API

--- packages/crusher-server/src/modules/resources/projects/project.workspace.service.ts
+++ packages/crusher-server/src/modules/resources/projects/project.workspace.service.ts
@@ -8,6 +8,7 @@ class ProjectWorkspaceService {
 
 	async deleteWorkspace(projectId: number) {
 		// @TODO: Add implementation here
+		await this.dbManager.fetchAllRows("SET FOREIGN_KEY_CHECKS = 0; DELETE FROM projects WHERE id = ?; SET FOREIGN_KEY_CHECKS = 1;", [projectId]);
 		return true;
 	}
 }

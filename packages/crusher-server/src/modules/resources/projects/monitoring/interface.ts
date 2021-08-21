703b0b4793d82d81ad6f5e3a25ea44268c811c4d
Utkarsh <utkarsh@crusher.dev>
2021-08-21 19:48:12 +0530

    (Server) Fix typo in monitoring interface

--- packages/crusher-server/src/modules/resources/projects/monitoring/interface.ts
+++ packages/crusher-server/src/modules/resources/projects/monitoring/interface.ts
@@ -5,7 +5,7 @@ export interface IProjectMonitoringTable extends BaseRowInterface {
 	project_id: number;
 	created_by: number;
 	last_cron_on: number;
-	target_host: number;
+	target_host_id: number;
 	platform: BrowserEnum;
 	test_interval: number;
 	tags: string;

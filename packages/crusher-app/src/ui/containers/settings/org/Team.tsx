6692caee974b0b94d8b1fb8380b3b6f2a44dcd40
Himanshu Dixit <hudixt@gmail.com>
2021-08-27 07:17:05 +0530

    (App): Add team members support

--- packages/crusher-app/src/ui/containers/settings/org/Team.tsx
+++ packages/crusher-app/src/ui/containers/settings/org/Team.tsx
@@ -9,9 +9,14 @@ import { Conditional } from "dyson/src/components/layouts";
 
 import InviteMember from "@ui/containers/dashboard/InviteMember";
 import { SettingsLayout } from "@ui/layout/SettingsBase";
+import useSWR from 'swr';
+import { backendRequest } from '@utils/backendRequest';
+import { resolvePathToBackendURI } from '@utils/url';
 
 export const TeamMembers = () => {
 	const [showModal, setShowModal] = useState(false);
+	const {data} = useSWR(resolvePathToBackendURI("/teams/users"))
+
 	return (
 		<SettingsLayout>
 			<Conditional showIf={showModal}>
@@ -20,7 +25,7 @@ export const TeamMembers = () => {
 			<div className={"text-24 mb-100"} css={maxWidthContainer}>
 				<div className={"flex justify-between items-start mt-16"}>
 					<div>
-						<Heading type={2} fontSize={16} className={"mb-12"}>
+						<Heading type={2} fontSize={16} className={"mb-8"}>
 							Team members
 						</Heading>
 						<TextBlock fontSize={13} className={"mb-24"}>
@@ -40,22 +45,30 @@ export const TeamMembers = () => {
 				</div>
 
 				<hr css={basicHR} />
-
-				<Card css={projectListCard}>
-					<div className={"flex justify-between items-center"}>
-						<div className={"text-15"}>fds</div>
-						<div className={"text-13"} id={"delete"}>
-							Delete
+				{data && data.map(({name,role,email})=>(
+					<Card css={projectListCard}>
+						<div className={"flex justify-between items-start"}>
+							<div className={"text-14"}>
+								<div>{name}</div>
+								<div className={"text-12"}>{email} | {role}</div>
+							</div>
+							<Conditional showIf={false}>
+								<div className={"text-12"} id={"delete"}>
+									Deactivate
+								</div>
+							</Conditional>
 						</div>
-					</div>
-				</Card>
+					</Card>
+				))}
+
+
 			</div>
 		</SettingsLayout>
 	);
 };
 
 const projectListCard = css`
-	padding: 12rem 20rem;
+	padding: 14rem 20rem;
 	#delete {
 		:hover {
 			text-decoration: underline;

3112401b5bf32cd70f283a2f0310d47b4c4d9d21
Himanshu Dixit <hudixt@gmail.com>
2021-08-30 09:57:16 +0530

    (Merge): Add monitoring option

--- packages/crusher-app/src/ui/containers/settings/project/Monitoring.tsx
+++ packages/crusher-app/src/ui/containers/settings/project/Monitoring.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react";
-import { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { Card } from "../../../../../../dyson/src/components/layouts/Card/Card";
 import { Button } from "dyson/src/components/atoms";
@@ -7,16 +7,190 @@ import { Heading } from "dyson/src/components/atoms/heading/Heading";
 import { TextBlock } from "dyson/src/components/atoms/textBlock/TextBlock";
 import { Conditional } from "dyson/src/components/layouts";
 
-import InviteMember from "@ui/containers/dashboard/InviteMember";
 import { SettingsLayout } from "@ui/layout/SettingsBase";
+import useSWR from "swr";
+import { useAtom } from "jotai";
+import { currentProject } from "../../../../store/atoms/global/project";
+import { createProjectMonitoring, getProjectMonitoring } from "@constants/api";
+import { ChevronRight } from "@svg/settings";
+import { atomWithImmer } from "jotai/immer";
+import { ChevronDown } from "@svg/testReport";
+import { Input } from "dyson/src/components/atoms/input/Input";
+import { LoadingSVG } from "@svg/dashboard";
+import { backendRequest } from "@utils/common/backendRequest";
+import { RequestMethod } from "../../../../types/RequestOptions";
+import { converServerToClientSideStateMonitoring, convertToServerSideMonitoring } from "@utils/core/settings/environmentSettingUtils";
+import { sendSnackBarEvent } from "@utils/common/notify";
+
+function MonitoringForm({ id }) {
+	const [project] = useAtom(currentProject);
+	const [monitoringInStore, setMonitoring] = useAtom(monitoringAtom);
+	const [savingEnv, setSavingEnv] = useState(false);
+	const { notSavedInDb, environmentId, testInterval } = monitoringInStore[id];
+
+	const setEnv = (e) => {
+		setMonitoring((monitoring) => {
+			monitoring[id].environmentId = e.target.value;
+		});
+	};
+
+	const setInterval = (e) => {
+		setMonitoring((monitorings) => {
+			monitorings[id].testInterval = e.target.value;
+		});
+	};
+
+	const saveInServer = async () => {
+		setSavingEnv(true);
+		const currentMonitoringData = monitoringInStore[id];
+		const payload = convertToServerSideMonitoring(currentMonitoringData);
+		await backendRequest(createProjectMonitoring(project.id), {
+			method: RequestMethod.POST,
+			payload,
+		});
+
+		setSavingEnv(false);
+	};
+
+	return (
+		<div className={"px-24"}>
+			<div className={"mt-12 flex justify-between text-13 items-center"}>
+				<div>Environment id</div>
+				<div>
+					<Input
+						css={css`
+							height: 36rem;
+						`}
+						placeholder={"https://google.com"}
+						onBlur={setEnv}
+						initialValue={environmentId}
+					/>
+				</div>
+			</div>
+
+			<div className={"mt-12 flex justify-between text-13 items-center"}>
+				<div>
+					testInterval <span className={"text-12 ml-8"}>In Sec</span>
+				</div>
+				<div>
+					<Input
+						css={css`
+							height: 36rem;
+						`}
+						placeholder={"https://google.com"}
+						onBlur={setInterval}
+						initialValue={testInterval}
+					/>
+				</div>
+			</div>
+
+			<div className={"flex justify-between items-center mt-40 mb-20 text-13"}>
+				<TextBlock
+					fontSize={13}
+					color={"#d7537b"}
+					css={css`
+						:hover {
+							text-decoration: underline;
+						}
+					`}
+				>
+					{!notSavedInDb ? "Delete" : ""}
+				</TextBlock>
+				<Button
+					bgColor={"tertiary-dark"}
+					css={css`
+						width: 120rem;
+					`}
+					onClick={saveInServer}
+					className={"flex items-center justify-center"}
+				>
+					<Conditional showIf={savingEnv}>
+						<LoadingSVG height={12} width={12} className={"mr-8"} />
+					</Conditional>
+					Save
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+function MonitoringCard({ monitoringData, id }) {
+	const { isOpen, testInterval, environmentId } = monitoringData;
+	const [environmentsInStore, setEnvironment] = useAtom(monitoringAtom);
+
+	const { notSavedInDB } = environmentsInStore[id];
+
+	const onClick = () => {
+		setEnvironment((environemnt) => {
+			if (notSavedInDB) {
+				sendSnackBarEvent({ type: "normal", message: "Please save new env before closing" });
+				return;
+			}
+
+			for (const env of environemnt) {
+				env.isOpen = false;
+			}
+			environemnt[id].isOpen = !isOpen;
+		});
+	};
+
+	return (
+		<Card css={projectListCard}>
+			<div className={"flex justify-between items-center"} onClick={onClick} id={"top-section"}>
+				<div className={"text-14"}>
+					{environmentId} - {testInterval}
+				</div>
+				<div className={"text-13"} id={"delete"}>
+					{isOpen ? <ChevronDown /> : <ChevronRight />}
+				</div>
+			</div>
+
+			<Conditional showIf={isOpen}>
+				<MonitoringForm id={id} />
+			</Conditional>
+		</Card>
+	);
+}
+
+type TMonitoringCard = {
+	id?: number;
+	environmentId?: number;
+	testInterval?: number;
+	isOpen: boolean;
+	notSavedInDB?: boolean;
+};
+
+/*
+	Move to environment atom
+ */
+const monitoringAtom = atomWithImmer<TMonitoringCard[]>([]);
 
 export const Monitoring = () => {
-	const [showModal, setShowModal] = useState(false);
+	const [project] = useAtom(currentProject);
+
+	const { data: monitoring } = useSWR(getProjectMonitoring(project.id));
+	const [monitoringInStore, setMonitoring] = useAtom(monitoringAtom);
+
+	useEffect(() => {
+		if (monitoring === undefined) return;
+		setMonitoring(converServerToClientSideStateMonitoring(monitoring));
+	}, [monitoring]);
+
+	const addEmptyEnvToStore = useCallback(() => {
+		setMonitoring([
+			...monitoringInStore,
+			{
+				environmentId: "",
+				testInterval: 3600,
+				isOpen: true,
+				notSavedInDB: true,
+			},
+		]);
+	}, [monitoringInStore]);
+
+	if (!monitoringInStore) return null;
 	return (
 		<SettingsLayout>
-			<Conditional showIf={showModal}>
-				<InviteMember onClose={setShowModal.bind(this, false)} />
-			</Conditional>
 			<div className={"text-24 mb-100"} css={maxWidthContainer}>
 				<div className={"flex justify-between items-start mt-16"}>
 					<div>
@@ -29,33 +203,36 @@ export const Monitoring = () => {
 					</div>
 					<div>
 						<Button
-							onClick={setShowModal.bind(this, true)}
+							onClick={addEmptyEnvToStore.bind(this)}
 							css={css`
 								width: 164rem;
 							`}
 						>
-							Add a member
+							Add Monitoring
 						</Button>
 					</div>
 				</div>
 
 				<hr css={basicHR} />
 
-				<Card css={projectListCard}>
-					<div className={"flex justify-between items-center"}>
-						<div className={"text-15"}>fds</div>
-						<div className={"text-13"} id={"delete"}>
-							Delete
-						</div>
-					</div>
-				</Card>
+				<Conditional showIf={monitoringInStore.length > 0}>
+					{monitoringInStore.map((monitoringData, i) => {
+						return <MonitoringCard monitoringData={monitoringData} key={i} id={i} />;
+					})}
+				</Conditional>
+				<Conditional showIf={monitoringInStore.length < 1}>
+					<div className={"text-13 mt-40"}>You don't have any monitoring yet in your project.</div>
+				</Conditional>
 			</div>
 		</SettingsLayout>
 	);
 };
 
 const projectListCard = css`
-	padding: 12rem 20rem;
+	padding: 0;
+	#top-section {
+		padding: 12rem 24rem;
+	}
 	#delete {
 		:hover {
 			text-decoration: underline;
--- packages/crusher-app/src/utils/core/settings/environmentSettingUtils.ts
+++ packages/crusher-app/src/utils/core/settings/environmentSettingUtils.ts
@@ -1,15 +1,63 @@
-import produce from 'immer';
+import produce from "immer";
 
-export const convertEnvToServerSide= (data)=>{
-	const newState = produce((draft)=>{
-		const varEntries = draft.vars.map(({variableName,variableValue})=>{
-			return [variableName.value,variableValue.value]
-		})
+export const convertEnvToServerSide = (data) => {
+	const newState = produce((draft) => {
+		const varEntries = draft.vars.map(({ variableName, variableValue }) => {
+			return [variableName.value, variableValue.value];
+		});
 
-		draft.vars = Object.fromEntries(varEntries)
-		delete draft.notSavedInDB
-		delete draft.isOpen
-	})(data)
+		draft.vars = Object.fromEntries(varEntries);
+		delete draft.notSavedInDB;
+		delete draft.isOpen;
+	})(data);
 
 	return newState;
-}
+};
+
+export const converServerToClientSideState = (data) => {
+	return data.map((dataItem) => {
+		const { id, name, browser, vars } = dataItem;
+
+		const parsedVars = Object.entries(JSON.parse(vars)).map(([k, v]) => {
+			return {
+				variableName: {
+					value: k,
+				},
+				variableValue: {
+					value: v,
+				},
+			};
+		});
+		return {
+			id,
+			name,
+			host: "",
+			browser,
+			isOpen: false,
+			vars: parsedVars,
+			notSavedInDB: false,
+		};
+	});
+};
+
+export const converServerToClientSideStateMonitoring = (data) => {
+	return data.map((dataItem) => {
+		const { id, testInterval, environmentId } = dataItem;
+		return {
+			id,
+			environmentId,
+			testInterval,
+			isOpen: false,
+			notSavedInDB: false,
+		};
+	});
+};
+
+export const convertToServerSideMonitoring = (data) => {
+	const newState = produce((draft) => {
+		delete draft.notSavedInDB;
+		delete draft.isOpen;
+	})(data);
+
+	return newState;
+};

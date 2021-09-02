0ff20e86089a448ba98edc26a5c29e38094acbae
Himanshu Dixit <hudixt@gmail.com>
2021-09-02 14:57:04 +0530

    (App): Change build api trigger key

--- packages/crusher-app/src/constants/api.ts
+++ packages/crusher-app/src/constants/api.ts
@@ -11,7 +11,7 @@ export const getBuildsList = (projectId: string, triggerType: BuildTriggerEnum, 
 	const urlParams = new URLSearchParams(filteredObj);
 	const urlParamString = urlParams.toString();
 
-	return resolvePathToBackendURI(`/projects/${projectId}/builds?${triggerType ? `trigger=${triggerType.toLowerCase()}` : ""}${urlParamString}`);
+	return resolvePathToBackendURI(`/projects/${projectId}/builds?${triggerType ? `triggerType=${triggerType.toLowerCase()}` : ""}${urlParamString}`);
 };
 
 export const getRunTestApi = (projectId: number) => resolvePathToBackendURI(`/projects/${projectId}/tests/actions/run`);

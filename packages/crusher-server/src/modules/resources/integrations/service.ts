acf3623a003e28bb23c3d7ab12dfa063cc9b9835
Utkarsh <utkarsh@crusher.dev>
2021-09-07 14:01:03 +0530

    (Server) Add API to get list of integrations

--- packages/crusher-server/src/modules/resources/integrations/service.ts
+++ packages/crusher-server/src/modules/resources/integrations/service.ts
@@ -1,20 +1,28 @@
+import { KeysToCamelCase } from "@modules/common/typescript/interface";
 import { DBManager } from "@modules/db";
+import { CamelizeResponse } from "@modules/decorators/camelizeResponse";
 import { SlackOAuthResponse } from "@modules/slack/interface";
 import { Inject, Service } from "typedi";
+import { IIntegrationsTable } from "./interface";
 
 @Service()
 class IntegrationsService {
 	@Inject()
 	private dbManager: DBManager;
 
-	async addSlackIntegration(integrationConfig: SlackOAuthResponse, userId: number) {
-		return this.dbManager.insert(`INSERT INTO user_integrations SET ?`, {
-			user_id: userId,
+	async addSlackIntegration(integrationConfig: SlackOAuthResponse, projectId: number) {
+		return this.dbManager.insert(`INSERT INTO integrations SET ?`, {
+			projectId: projectId,
 			label: `${integrationConfig.team.name}___${integrationConfig.incoming_webhook.channel}`,
 			access_token: integrationConfig.access_token,
 			webhook_url: integrationConfig.incoming_webhook.url,
 			meta: JSON.stringify(integrationConfig),
 		});
+	}
+
+	@CamelizeResponse()
+	async getListOfIntegrations(projectId: number): Promise<Array<KeysToCamelCase<IIntegrationsTable>>> {
+		return this.dbManager.fetchAllRows("SELECT * FROM integrations WHERE project_id = ?", [projectId]);
 	}
 }
 

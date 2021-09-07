a8480aa7ee6a95d3ff6b634c6b2f1b1fad55a8b3
Utkarsh <utkarsh@crusher.dev>
2021-09-07 14:01:03 +0530

    (Server) Add API to get list of integrations

--- packages/crusher-server/src/modules/resources/integrations/controller.ts
+++ packages/crusher-server/src/modules/resources/integrations/controller.ts
@@ -2,6 +2,7 @@ import { SlackService } from "@modules/slack/service";
 import { Authorized, CurrentUser, Get, JsonController, Param, Post, QueryParams, Res } from "routing-controllers";
 import { Inject, Service } from "typedi";
 import { AlertingService } from "../alerting/service";
+import { IntegrationServiceEnum } from "./interface";
 import { IntegrationsService } from "./service";
 
 @Service()
@@ -20,7 +21,7 @@ class IntegrationsController {
 		const { code: slackCode, state: projectId } = params;
 
 		const integrationConfig = await this.slackService.verifySlackIntegrationRequest(slackCode);
-		const slackIntegrationRecord = await this.integrationsService.addSlackIntegration(integrationConfig, user.user_id);
+		const slackIntegrationRecord = await this.integrationsService.addSlackIntegration(integrationConfig, projectId);
 
 		await this.projectAlertingService.addAlertIntegrationToProject({
 			integrationId: slackIntegrationRecord.insert_id,
@@ -31,6 +32,18 @@ class IntegrationsController {
 
 		// @TODO: Add a redirect here
 		return "Successful";
+	}
+
+	@Authorized()
+	@Get("/integrations/:project_id")
+	async getIntegrations(@CurrentUser({ required: true }) user, @Param("project_id") projectId: number) {
+		const integrationsList = await this.integrationsService.getListOfIntegrations(projectId);
+		const isSlackIntegrated = integrationsList.findIndex((item) => item.integrationName === IntegrationServiceEnum.SLACK);
+
+		return {
+			emailIntegration: true,
+			slackIntegration: isSlackIntegrated !== -1,
+		};
 	}
 }
 

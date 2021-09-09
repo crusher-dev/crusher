71bcbf30056554f1f74749e792264771bb7366a2
Himanshu Dixit <hudixt@gmail.com>
2021-09-09 23:18:07 +0530

    (docs): change readme (#154)

--- packages/crusher-app/src/constants/app.ts
+++ packages/crusher-app/src/constants/app.ts
@@ -6,7 +6,7 @@ export type TOSDownloadInfo = {
 
 export const OS_INFO: Record<string, TOSDownloadInfo> = {
 	Windows: { downloadLink: "", icon: null, label: "Download exe" },
-	MAC: { downloadLink: "https://github.com/crusherdev/crusher-downloads/blob/master/v1/crusher-recorder.deb?raw=true", icon: null, label: "Download dmg" },
+	MAC: { downloadLink: "https://github.com/crusherdev/crusher-downloads/blob/master/v1/crusher-recorder.dmg?raw=true", icon: null, label: "Download dmg" },
 	OTHER: { downloadLink: "", icon: null },
 };
 

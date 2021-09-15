c193e851037de0a9a77a127a7e2168daed100b2f
Himanshu Dixit <hudixt@gmail.com>
2021-09-15 22:16:46 +0530

    (app): Fix server.js

--- packages/crusher-app/server.js
+++ packages/crusher-app/server.js
@@ -7,25 +7,64 @@ const express = require("express");
 const proxy = require("express-http-proxy");
 const port = process.env.port || 3000;
 const server = express();
+const fs = require("fs")
 
 const bodyParser = require("body-parser");
 
-	server.use(bodyParser({ limit: "50mb" }));
-	server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser({ limit: "50mb" }));
+server.use(bodyParser.urlencoded({ extended: false }));
 
-	server.use("/server", proxy("localhost:8000"));
+server.use("/server", proxy("localhost:8000"));
 
-	// Expose storage folder if using local storage
-	// (Default Storage Method in OSS)
-	if (process.env.NEXT_PUBLIC_CRUSHER_MODE === "open-source") {
-		server.use("/output", proxy("localhost:3001"));
+// Expose storage folder if using local storage
+// (Default Storage Method in OSS)
+if (process.env.NEXT_PUBLIC_CRUSHER_MODE === "open-source") {
+	server.use("/output", proxy("localhost:3001"));
+}
+
+server.use(async (req,res,next)=>{
+	if(req.url === "" || req.url === "/"){
+		const filePath = "out/index.html"
+		serveFile(filePath, res)
+		return;
 	}
 
-	// This is currently used for server
-	server.use("*", express.static("out"));
+	try{
+		console.log("out"+getSanitizedPath(req.url))
+		const data  = await fs.readFileSync("out"+getSanitizedPath(req.url)+".html");
+		if(data !== null){
+			res.end(data)
+		}
+		next()
+	}
+	catch (e) {
+		next()
+	}
 
-	server.listen(port, (err) => {
-		if (err) throw err;
-		//eslint-disable-next-line
-		console.log(`> Ready on http://localhost:${port}`);
+
+})
+// This is currently used for server
+server.use("/", express.static("out"));
+
+server.listen(port, (err) => {
+	if (err) throw err;
+	//eslint-disable-next-line
+	console.log(`> Ready on http://localhost:${port}`);
+});
+
+
+
+function serveFile(filePath, res){
+	fs.readFile(filePath, function(err, data) {
+
+		res.end(data);
 	});
+}
+
+function getSanitizedPath(path){
+	if(path.charAt(path.length-1) === "/"){
+		return path.substr(0,path.length-1)
+	}
+
+	return path;
+}

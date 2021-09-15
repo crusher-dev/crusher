1eb961c2dd3be578036ecfec81ba8bcddf03931c
Himanshu Dixit <hudixt@gmail.com>
2021-09-15 18:46:13 +0530

    (app): Fix FE prod build

--- packages/crusher-app/server.js
+++ packages/crusher-app/server.js
@@ -1,16 +1,15 @@
+// Valid only for production build.
+// For dev, use next dev
+
 require("dotenv").config();
 // Created this for proxy. Not be used in other cases
 const express = require("express");
-const next = require("next");
 const proxy = require("express-http-proxy");
 const port = process.env.port || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const server = express();
+
 const bodyParser = require("body-parser");
 
-app.prepare().then(() => {
-	const server = express();
 	server.use(bodyParser({ limit: "50mb" }));
 	server.use(bodyParser.urlencoded({ extended: false }));
 
@@ -22,11 +21,8 @@ app.prepare().then(() => {
 		server.use("/output", proxy("localhost:3001"));
 	}
 
-	// // This is currently used for
-	server.use("/assets", express.static(".next/public/assets"));
-	server.use("/js", express.static(".next/public/js"));
-	server.use("/lotties", express.static(".next/public/lotties"));
-	server.use("/svg", express.static(".next/public/svg"));
+	// This is currently used for server
+	server.use("/", express.static("out"));
 	server.all("*", (req, res) => {
 		return handle(req, res);
 	});
@@ -36,4 +32,3 @@ app.prepare().then(() => {
 		//eslint-disable-next-line
 		console.log(`> Ready on http://localhost:${port}`);
 	});
-});

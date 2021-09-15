da9cd584a6574d64c9e4bfa33f8e75f25b1e9a52
Himanshu Dixit <hudixt@gmail.com>
2021-09-15 18:46:13 +0530

    (app): Fix FE prod build

--- scripts/build/build-app.sh
+++ scripts/build/build-app.sh
@@ -1,1 +1,7 @@
-cd packages/crusher-app && yarn build && cd ../../
+cd packages/crusher-app && yarn build && yarn export
+rm -rf ../../output/crusher-app/out && mv out ../../output/crusher-app/out
+
+cd ../../
+
+
+rm -rf output/cruhser-app/.next

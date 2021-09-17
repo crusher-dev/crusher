cea5e7c1f7a6ad91f71b570ddc945090cc7b6532
Utkarsh <utkarsh@crusher.dev>
2021-09-17 09:13:11 +0530

    (TR) Add fetch method

--- packages/crusher-shared/types/sdk/sdk.ts
+++ packages/crusher-shared/types/sdk/sdk.ts
@@ -6,4 +6,5 @@ export interface ICrusherSdk {
 	reloadPage: () => Promise<boolean>;
 	sleep: (timeout: number) => Promise<boolean>;
 	setCookies: (cookies: Array<CrusherCookieSetPayload>) => Promise<boolean>;
+	fetch?: (uri: string, options: any) => Promise<boolean>;
 }

fde42ce5c17978bc00c9210de262127e8887bde0
Utkarsh <utkarsh@crusher.dev>
2021-10-01 11:47:16 +0530

    (TE) Add magic number in wait for navigation

--- packages/runner-utils/src/actions/waitForNavigation.ts
+++ packages/runner-utils/src/actions/waitForNavigation.ts
@@ -1,15 +1,16 @@
 import { ActionsInTestEnum } from "@crusher-shared/constants/recordedActions";
 import { iAction } from "@crusher-shared/types/action";
 import { Page } from "playwright";
+import { sleep } from "src/functions";
 
 async function waitForNavigation(page: Page, action: iAction) {
 	console.log("Waiting for navigation now...");
 	try {
-		await page.waitForNavigation();
+		await sleep(2); // @TODO: Add a magic number here
 		await page.waitForLoadState("networkidle");
 		await page.waitForLoadState("load");
 		await page.waitForLoadState("domcontentloaded");
-	} catch (ex) { console.error("Gt error here", ex); }
+	} catch (ex) { console.error("Gt error here", ex); await sleep(2); // Magic number }
 	console.log("Finsihed navigation");
 }
 

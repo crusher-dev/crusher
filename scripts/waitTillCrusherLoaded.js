56a2c0c4389c1843ff9fb0abd6f0f9452702e9a0
Utkarsh <utkarsh@crusher.dev>
2021-10-07 20:12:32 +0530

    (CI) Add script to wait for layerci server to boot

---
+++ scripts/waitTillCrusherLoaded.js
@@ -0,0 +1,59 @@
+const fetch = require("node-fetch");
+const yargs = require('yargs');
+const installOptions = {};
+
+function preParseArgv() {
+	const argv = yargs
+		.options({
+		  url: {
+				describe: 'Server Url of crusher deployment',
+				string: true,
+      },
+		})
+		.help()
+		.alias('help', 'h').argv;
+
+
+	if (argv.url) {
+		installOptions['url'] = argv.url;
+  }
+}
+
+
+async function waitForWebsite(url) {
+  return new Promise(async (resolve, reject) => {
+    let startingOffsetCount = 0;
+
+    let interval = null;
+
+    const waitFunc = async () => {
+      if (startingOffsetCount > 60) { if (interval) clearInterval(interval); resolve(false); return false; }
+      console.log(`Pinging ${url} now...`);
+      const res = await fetch(url, { method: "GET" });
+      const responseText = await res.text();
+      if (responseText.includes("Cannot GET /")) {
+        if (interval) clearInterval(interval);
+        resolve(true);
+        return true;
+      }
+      startingOffsetCount++;
+    };
+
+    if (await waitFunc()) return resolve(true);
+
+    interval = setInterval(waitFunc, 5000);
+  });
+}
+
+async function boot() {
+  preParseArgv();
+  if (!installOptions.url) {
+    console.error("No crusher server deployment url provided");
+    process.exit(1);
+  }
+
+  await waitForWebsite(installOptions.url);
+}
+
+
+boot();

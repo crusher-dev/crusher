3951e1073bd3889d96f566171f781535ac647524
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-10-14 14:54:23 +0530

    (FE,BE) Add Forgot Password (#171)

---
+++ packages/crusher-app/pages/forgot_password.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container  } from "@ui/containers/forgot_password/index";
+
+import { usePageTitle } from "../src/hooks/seo";
+
+function LoginPage() {
+	usePageTitle("Forgot Password");
+
+	return (
+		<div>
+			<Container />
+		</div>
+	);
+}
+
+export default LoginPage;

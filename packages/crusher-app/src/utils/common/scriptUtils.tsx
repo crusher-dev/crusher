01be9c116429206a5dcecc4eda62e6f0cb25708d
Himanshu <hudixt@gmail.com>
2021-10-18 17:14:52 +0530

    (app): Add segment tracking

--- packages/crusher-app/src/utils/common/scriptUtils.tsx
+++ packages/crusher-app/src/utils/common/scriptUtils.tsx
@@ -9,13 +9,14 @@ export function addScript(id, attribute, text, callback) {
 	document.body.appendChild(s);
 }
 
+const SEGMENT_KEY = process.env.NEXT_PUBLIC_SEGMENT_KEY || "N37AcGJzsY7gUkuE0kaaoxgJeunzilx1"
 export const loadSegment = () => {
 	addScript(
 		"track",
 		{},
 		`
-  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="N37AcGJzsY7gUkuE0kaaoxgJeunzilx1";;analytics.SNIPPET_VERSION="4.15.3";
-  analytics.load("N37AcGJzsY7gUkuE0kaaoxgJeunzilx1");
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${SEGMENT_KEY}";;analytics.SNIPPET_VERSION="4.15.3";
+  analytics.load("${SEGMENT_KEY}");
   analytics.page();
   }}();
 	`,

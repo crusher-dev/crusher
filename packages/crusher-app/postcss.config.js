cca2dbbb7f5f7387e0d276414c56a90185362c03
Himanshu <hudixt@gmail.com>
2021-11-11 18:45:21 +0530

    (Fix): S{

--- packages/crusher-app/postcss.config.js
+++ packages/crusher-app/postcss.config.js
@@ -1,13 +1,13 @@
 module.exports = {
 	plugins: [
 		"tailwindcss",
-		[
-			'@fullhuman/postcss-purgecss',
-			{
-				content: ["./src/**/*.jsx", "./src/**/*.tsx", "./pages/**/*.tsx", "./pages/**/*.jsx"],
-				defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-				safelist: ["html", "body"]
-			}
-		],
+		// [
+		// 	'@fullhuman/postcss-purgecss',
+		// 	{
+		// 		content: ["./src/**/*.jsx", "./src/**/*.tsx", "./pages/**/*.tsx", "./pages/**/*.jsx"],
+		// 		defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+		// 		safelist: ["html", "body"]
+		// 	}
+		// ],
 	],
 };

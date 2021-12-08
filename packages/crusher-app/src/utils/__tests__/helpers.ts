bb71668e430c4e191fc7a440772ead279d297621
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-12-08 11:47:34 +0530

    Unit test

--- packages/crusher-app/src/utils/__tests__/helpers.ts
+++ packages/crusher-app/src/utils/__tests__/helpers.ts
@@ -1,37 +1,27 @@
-import { getCollapseList } from '../helpers'
+import { groupTestResults } from '../helpers'
 
 
-test('should ', () => {
-    expect(getCollapseList([{}])).toMatchObject({"lastStep": 1, "remainingSteps": -1});
-    expect(getCollapseList([
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
+test('groupTestResults ', () => {
+    expect(groupTestResults([
+        { status: "COMPLETE" },
+        { status: "COMPLETE" },
         { status: "FAILED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-    ])).toMatchObject({ lastStep: 2, remainingSteps: 2 });
-    expect(getCollapseList([
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "FAILED" },
-    ])).toMatchObject({ lastStep: 4, remainingSteps: 2 });
-    expect(getCollapseList([
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "FAILED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-    ])).toMatchObject({ lastStep: 4, remainingSteps: 5 });
-    expect(getCollapseList([
-        { status: "FAILED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-        { status: "COMPLETED" },
-    ])).toMatchObject({ lastStep: 3, remainingSteps: 2 });
+        { status: "COMPLETE" },
+        { status: "REVIEW" },
+        { status: "COMPLETE", },
+        { status: "COMPLETE", },
+        { status: "COMPLETE", },
+        { status: "FAILED", },
+        { status: "COMPLETE", },
+        { status: "COMPLETE", },
+        { status: "COMPLETE", },
+    ])).toMatchObject([
+        { type: 'show', from: 0, to: 2, count: 3 },
+        { type: 'hide', from: 3, to: 3, count: 1 },
+        { type: 'show', from: 4, to: 4, count: 1 },
+        { type: 'hide', from: 5, to: 7, count: 3 },
+        { type: 'show', from: 8, to: 8, count: 1 },
+        { type: 'hide', from: 9, to: 11, count: 3 }
+      ]);
 
 })

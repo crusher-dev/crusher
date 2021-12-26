7cbba866feca58e69d87b6d38420e34e6fa0d2ad
Utkarsh <utkarshdix02@gmail.com>
2021-12-26 14:26:11 +0530

    .

--- packages/electron-app/src/preload/webview.ts
+++ packages/electron-app/src/preload/webview.ts
@@ -1,6 +1,7 @@
 // Preload (Isolated World)
 import { contextBridge, ipcRenderer } from "electron";
 
+contextBridge.exposeInMainWorld("isCrusherRecorder", true);
 
 contextBridge.exposeInMainWorld("recorder", {
     sendMessage: async(...args) => { console.log("Sending data"); ipcRenderer.sendToHost("recorder-message", ...args) },

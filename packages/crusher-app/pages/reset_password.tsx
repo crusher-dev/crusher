0a7662434bb084517dfcdc14a220eaa6b9993e27
Arpit Bhalla <arpitbhalla2001@gmail.com>
2022-01-05 21:31:18 +0530

    reset password

--- packages/crusher-app/pages/reset_password.tsx
+++ packages/crusher-app/pages/reset_password.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ResetPasswordContainer } from "@ui/containers/reset_password";
+import ResetPasswordContainer from "@ui/containers/auth/reset";
 import { usePageTitle } from "../src/hooks/seo";
 
 function ResetPassPage() {

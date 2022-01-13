b4d5a4f61cfebdd1d4ffaffdcac2caad3fc17bc1
Utkarsh <utkarshdix02@gmail.com>
2022-01-13 18:53:19 +0530

    .

--- packages/electron-app/src/ui/components/sidebar/index.tsx
+++ packages/electron-app/src/ui/components/sidebar/index.tsx
@@ -6,9 +6,7 @@ import { Conditional } from "@dyson/components/layouts";
 import { ActionsPanel } from "./actionsPanel";
 import { StepsPanel } from "./steps";
 import { TemplatesModal } from "./steps/templatesModal";
-import { Tooltip } from "@dyson/components/atoms/";
-import { CrossIcon } from "../../icons";
-import { StepInfoEditor } from "./steps/stepInfoEditor";
+
 const Sidebar = ({className, ...props}: any) => {
     const recorderInfo = useSelector(getRecorderInfo);
 

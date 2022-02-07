e5676810d5f58db8a73aa6e0f0646ec640aed1a7
Utkarsh Dixit <utkarsh@crusher.dev>
2022-02-07 07:31:03 +0530

    Update bootstrap.ts

--- packages/video-processor/ee/bootstrap.ts
+++ packages/video-processor/ee/bootstrap.ts
@@ -114,6 +114,10 @@ class EnterpriseVideoProcessorBootstrap extends VideoProcessorBootstrap {
 							console.error("Recieved error while shutting down", err);
 						});
 				}
+			} else {
+				 console.log("Processing size:", (this._worker as any).processing.size);
+				 console.log("Processing map:", (this._worker as any).processing);
+				 console.log("Time gap:", Date.now() - this._lastJobPickedUpTime, this._lastJobPickedUpTime);
 			}
 		}, 60000);
 	}

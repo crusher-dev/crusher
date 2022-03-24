ff4204cbfe516380a2fa5bb73c38d86ad95f5b19
Utkarsh <utkarshdix02@gmail.com>
2022-03-24 19:48:16 +0530

    (EA) Refractor

--- packages/electron-app/src/ui/components/device-frame/index.tsx
+++ packages/electron-app/src/ui/components/device-frame/index.tsx
@@ -13,8 +13,8 @@ import { TRecorderCrashState, TRecorderState } from "electron-app/src/store/redu
 import { InfoOverLay } from "../overlays/infoOverlay";
 import { MiniCrossIcon, StopIcon } from "../../icons";
 import { Button } from "@dyson/components/atoms";
-import { getLogs } from "electron-app/src/store/selectors/logger";
 import { ILoggerReducer } from "electron-app/src/store/reducers/logger";
+import { StatusBar } from "../status-bar";
 
 const CrashScreen = (props: any) => {
 	const store = useStore();
@@ -96,126 +96,6 @@ const PageLoadFailedScreen = (props: any) => {
 	);
 };
 
-function formatLogs(logs: Array<ILoggerReducer["logs"][0]>): Array<ILoggerReducer["logs"][0]> {
-	logs = logs.map((log, index) => { return {...log, diff: index == 0 ? "0" : (log.time - logs[index - 1].time).toFixed(2)}});
-	const noParentLogs = logs.filter((log: ILoggerReducer["logs"][0]) => !log.parent);
-	return noParentLogs.map((log) => {
-		return {...log, children: logs.filter((_log: ILoggerReducer["logs"][0]) => _log.parent === log.id)};
-	})
-}
-
-const StatusBar = (props: any) => {
-	const [clicked, setClicked] = React.useState(false);
-	const logsScrollRef: React.Ref<HTMLDivElement> = React.useRef(null);
-	const logs = useSelector(getLogs);
-
-	React.useEffect(() => {
-		if(logs && logs.length) {
-			const listContainer: any = document.querySelector("#logs-list");
-			if(listContainer) {
-				const elementHeight = listContainer.scrollHeight;
-				listContainer.scrollBy(0, elementHeight);
-			}
-		}
-	}, [logs]);
-
-	const LogItem = (props: {log: ILoggerReducer["logs"][0] & {children: Array<ILoggerReducer["logs"][0]>; diff: string; }, diff: string; className?: string; }) => {
-		const {log} = props;
-
-		return (
-			<div className={`${props.className}`}>
-		<div css={css`padding-top: 16rem; display: flex;`}>
-			<div>
-				<span css={css`font-size: 14rem; color: ${log.type === "error" ? "#C2607D":  "#9FC370"}; font-family: Gilroy;`}>{log.type}</span>
-				<span css={css`font-size: 14rem; color: #717171; font-family: Gilroy; word-break: break-all;`} className={"ml-20"}>{log.message}</span>
-			</div>
-			<div css={css`margin-left: auto;`}>
-				<span css={css`color: #525252; font-size: 12.5rem; font-family: Gilroy;`}>+{props.diff} ms</span>
-			</div>
-		</div>
-			<Conditional showIf={log.children && log.children.length}>
-				{log.children && log.children.map((child: ILoggerReducer["logs"][0] & {children: Array<ILoggerReducer["logs"][0]>; diff: string; }) => {
-					return <LogItem css={css`padding-left: 20rem;`} key={child.id} log={child} diff={child.diff}/>
-				})}
-			</Conditional>
-		</div>
-		)
-	};
-
-	const lastLogMessage = logs && logs.length ? logs[logs.length - 1].message : "";
-	return (
-	<>
-		<div id={`logsTab`} className={`${clicked ? "expandBar" : ""}`} css={statusBarContainerStyle}>
-			<div css={css`display: flex; align-items: center; height: 100%; max-height: 32rem; 	padding: 0rem 14rem;     border-bottom: 1px solid #2c2c2c;`}>
-				<div onClick={setClicked.bind(this, !clicked)} css={statusBarTabStyle}>Logs <span className="ml-4" css={logsCountStyle}>{logs && logs.length ? logs.length : 0}</span></div>
-
-				<Conditional showIf={!clicked}>
-					<div css={logTextStyle} className={"ml-20"}>{lastLogMessage.length > 100 ? lastLogMessage.substr(0, 100) + "..." : lastLogMessage}</div>
-				</Conditional>
-
-				<Conditional showIf={clicked}>
-					<div css={css`margin-left: auto;`}>
-						<div onClick={setClicked.bind(this, !clicked)} css={css`padding: 4rem 5rem; :hover { svg{ opacity: 0.7; } background: rgba(0, 0, 0, 0.2); opacity: 0.8; }`}>
-							<MiniCrossIcon css={css`width: 10rem; height: 10rem; opacity: 0.44; `}/>
-						</div>
-					</div>
-				</Conditional>
-			</div>
-
-			<Conditional showIf={clicked}>
-				<div id={"logs-list"} css={css`color: #fff; font-size: 14rem; padding: 0rem 14rem; padding-bottom: 8rem; height: calc(100% - 32rem); overflow-y: auto;`} className={"custom-scroll"}>
-					{logs && logs.length && formatLogs(logs).map((log: ILoggerReducer["logs"][0], index: number) => {
-						return <LogItem diff={log.diff} log={log} key={log.id}/>	
-					})}
-				</div>
-			</Conditional>
-		</div> 
-
-		<style>{`
-			.expandBar {
-				max-height: 312rem;
-			}
-		`}
-		</style>
-	</>
-	)
-};
-
-const logTextStyle = css`
-	color: #717171;
-	font-size: 13.5rem;
-	font-family: Gilroy;
-`;
-const logsCountStyle = css`
-	background: rgba(196, 196, 196, 0.1);
-	border-radius: 6rem;
-	font-size: 12rem;
-	font-family: Cera Pro;
-	color: rgba(255,255,255,0.7);
-	padding: 4rem 6rem;
-`;
-
-const statusBarTabStyle = css`
-	font-size: 14rem;
-	font-family: Cera Pro;
-	color: #8568D5;
-
-	:hover {
-		opacity: 0.8;
-	}
-`;
-const statusBarContainerStyle = css`
-	background: linear-gradient(0deg, rgba(255, 255, 255, 0.02), rgba(255, 255, 255, 0.02)), #0F1010;
-	border: 1px solid #272D2D;
-	width: 100%;
-    max-height: 32rem;
-	height: 100%;
-	position: absolute;
-	bottom: 0rem;
-	transition: max-height 0.1s;  
-	z-index: 999;
-`;
-
 const DeviceFrame = (props: any) => {
 	const recorderInfo = useSelector(getRecorderInfo);
 	const recorderState = useSelector(getRecorderState);
@@ -273,7 +153,7 @@ const DeviceFrame = (props: any) => {
 	}, [ref.current]);
 
 	return (
-		<div css={css`position: relative; overflow: hidden;`}>
+		<div css={topContainerStyle}>
 			<div css={containerStyle}>
 				<Conditional showIf={!!recorderInfo.device}>
 					<div style={{ width: `${recorderInfo.device?.width}rem`, height: `${recorderInfo.device?.height}rem`, maxWidth: "100%", maxHeight: "100%", position: "relative" }}>
@@ -297,17 +177,7 @@ const DeviceFrame = (props: any) => {
 							<PageLoadFailedScreen />
 						</Conditional>
 						<Conditional showIf={[TRecorderState.PERFORMING_ACTIONS, TRecorderState.PERFORMING_RECORDER_ACTIONS].includes(recorderState.type)}>
-							<div
-								css={css`
-									width: 100%;
-									height: 100%;
-									background: transparent;
-									position: absolute;
-									left: 0;
-									top: 0;
-									z-index: 999;
-								`}
-							></div>
+							<div css={deviceOverlayStyle}></div>
 						</Conditional>
 					</div>
 				</Conditional>
@@ -326,7 +196,15 @@ const buttonStyle = css`
 	width: 112rem;
 	height: 30rem;
 `;
-
+const deviceOverlayStyle = css`
+	width: 100%;
+	height: 100%;
+	background: transparent;
+	position: absolute;
+	left: 0;
+	top: 0;
+	z-index: 999;
+`;
 
 const runningStatusStyle = css`
 	position: fixed;
@@ -352,6 +230,7 @@ const webviewStyle = css`
 	height: 100%;
 	background: #fff;
 `;
+const topContainerStyle = css`position: relative; overflow: hidden;`;
 const containerStyle = css`
 	width: 100%;
 	height: 100%;

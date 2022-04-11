1f7601cfcecf2697f9a81945ad7c249ae64abb00
Utkarsh <utkarshdix02@gmail.com>
2022-04-12 01:48:24 +0530

    .

--- packages/test-runner/src/worker/runner.service.ts
+++ packages/test-runner/src/worker/runner.service.ts
@@ -46,7 +46,7 @@ export class CodeRunnerService {
 			assetsDir: identifer,
 			videoSavePath: `/tmp/crusher/${identifer}`,
 			defaultBrowserLaunchOptions: {
-				headless: false,
+				headless: true,
 				args: runnerConfig.browser === BrowserEnum.SAFARI ? [] : ["--disable-dev-shm-usage", "--disable-gpu"],
 				executablePath: isOpenSource() ? process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH : undefined,
 			},

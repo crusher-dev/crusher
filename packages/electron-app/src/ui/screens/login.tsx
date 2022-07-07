0dd46642bf4f28aab4e74625c3471565f905fe8e
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-07-07 15:48:57 +0530

    (EA) Update login screen

--- packages/electron-app/src/ui/screens/login.tsx
+++ packages/electron-app/src/ui/screens/login.tsx
@@ -180,7 +180,7 @@ function LoginScreen() {
 		});
 	}, []);
 
-	const handleLoginWithGithub = React.useCallback(async () => {
+	const handleLogin = React.useCallback(async () => {
 		if (interval) {
 			clearInterval(interval);
 			setInterval(null);
@@ -199,23 +199,6 @@ function LoginScreen() {
 		await shell.openExternal(resolveToFrontEndPath("?lK=" + loginKey, frontendEndPoint));
 	}, [interval]);
 
-	const handleLoginWithGitlab = React.useCallback(async () => {
-		if (interval) {
-			clearInterval(interval);
-			setInterval(null);
-		}
-		const { loginKey, interval: intervalMain } = await waitForUserLogin((loginToken: string) => {
-			setInterval(null);
-			saveAndGetUserInfo(loginToken).then((info) => {
-				focusOnWindow();
-				sendSnackBarEvent({ type: "success", message: `Login successful! Welcome, ${info.name}` });
-				handlePostLogin();
-			});
-		}, backendEndPoint);
-		setInterval(intervalMain);
-		setLoginText(resolveToFrontEndPath("?lK=" + loginKey, frontendEndPoint));
-		await shell.openExternal(resolveToFrontEndPath("?lK=" + loginKey, frontendEndPoint));
-	}, [interval]);
 
 	return (
 		<ModelContainerLayout
@@ -232,18 +215,23 @@ function LoginScreen() {
 						text-align: center;
 					`}
 				>
+					<div css={css`display: flex; justify-content: center;`}>
+						<UserProfileIcon css={css`width: 29rem; height: 29rem;`}/>
+					</div>
+
 					<div css={headingStyle}>Login to continue</div>
-					<div css={descriptionStyle}>This to save report, run test.</div>
+					<div css={descriptionStyle}>change login info</div>
 				</div>
 				<div
 					css={css`
 						display: flex;
 						gap: 22rem;
-						margin-top: 46rem;
+						margin-top: 18rem;
 					`}
 				>
-					<GithubButton onClick={handleLoginWithGithub} />
-					<GitlabButton onClick={handleLoginWithGitlab} />
+					<div css={loginButtonCss} onClick={handleLogin}>
+						login
+					</div>
 				</div>
 				{interval && (
 					<div
@@ -270,27 +258,68 @@ function LoginScreen() {
 						/>
 					</div>
 				)}
-
-				<div
-					css={css`
-						margin-top: 68rem;
-						display: flex;
-						align-items: center;
-						gap: 22rem;
-					`}
-				>
-					<span css={openThisLinkStyle}>or open this link</span>
-					<LinkBox
-						value={loginText}
-						css={css`
-							width: 220rem;
-						`}
-					></LinkBox>
-				</div>
 			</div>
 		</ModelContainerLayout>
 	);
 }
+
+const loginButtonCss = css`
+	background: linear-gradient(0deg, #933EFF, #933EFF), #4D4D4D;
+	border: 1px solid rgba(114, 114, 114, 0.4);
+	border-radius: 8px;
+	padding: 6rem;
+	width: 125rem;
+	font-family: Gilroy;
+	font-style: normal;
+	font-weight: 600;
+	font-size: 14rem;
+
+	color: #FFFFFF;
+	display: flex;
+	justify-content: center;
+	:hover {
+		opacity: 0.8;
+	}
+`;
+const UserProfileIcon = (props) => (
+	<svg
+      viewBox={"0 0 29 29"}
+	  fill="none"
+	  xmlns="http://www.w3.org/2000/svg"
+	  {...props}
+	>
+	  <path
+		opacity={0.1}
+		fillRule="evenodd"
+		clipRule="evenodd"
+		d="M1 14.114C1 3.314 3.35 1 14.313 1c10.962 0 13.312 2.315 13.312 13.114 0 6.28-.795 9.692-3.617 11.446l-.466-1.424c-.496-1.76-1.4-2.929-2.925-3.635-1.492-.691-3.547-.923-6.305-.923-2.76 0-4.814.25-6.307.961-1.523.725-2.426 1.91-2.921 3.668l-.46 1.358C1.796 23.812 1 20.4 1 14.114ZM9.505 11.2c0-2.616 2.152-4.736 4.807-4.736 2.656 0 4.808 2.12 4.808 4.736 0 2.615-2.152 4.735-4.808 4.735-2.654 0-4.807-2.12-4.807-4.735Z"
+		fill="#121418"
+	  />
+	  <path
+		d="M1 14.114C1 3.314 3.35 1 14.313 1c10.962 0 13.312 2.315 13.312 13.114 0 10.799-2.35 13.114-13.313 13.114C3.35 27.228 1 24.913 1 14.114Z"
+		fill="#121418"
+		stroke="#989898"
+		strokeWidth={2}
+	  />
+	  <path
+		d="M18.75 11.2c0 2.414-1.987 4.37-4.438 4.37-2.45 0-4.437-1.956-4.437-4.37 0-2.415 1.987-4.372 4.438-4.372 2.45 0 4.437 1.957 4.437 4.371Z"
+		fill="#121418"
+		stroke="#989898"
+		strokeWidth={2}
+	  />
+	  <path
+		d="M5.438 24.314c.943-3.362 3.372-4.372 8.875-4.372 5.502 0 7.93.937 8.874 4.298"
+		fill="#121418"
+	  />
+	  <path
+		d="M5.438 24.314c.943-3.362 3.372-4.372 8.875-4.372 5.502 0 7.93.937 8.874 4.298"
+		stroke="#989898"
+		strokeWidth={2}
+		strokeLinecap="round"
+	  />
+	</svg>
+  )
+
 
 const openThisLinkStyle = css`
 	font-family: "Gilroy";
@@ -306,15 +335,15 @@ const headingStyle = css`
 	font-family: Cera Pro;
 	font-style: normal;
 	font-weight: 700;
-	font-size: 16rem;
-
+	font-size: 18rem;
+	margin-top: 14rem;
 	color: #ffffff;
 `;
 const descriptionStyle = css`
 	font-family: Gilroy;
 	font-style: normal;
 	font-weight: 400;
-	font-size: 12rem;
+	font-size: 13.5rem;
 	text-align: center;
 	letter-spacing: -0.0032em;
 

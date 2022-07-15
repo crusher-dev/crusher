16b4a7cf27009447758e23aa63236cbf6eac1597
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-07-16 03:03:51 +0530

    (Runner Utils) Cleanup proxy middleware

--- packages/runner-utils/src/utils/proxy.ts
+++ packages/runner-utils/src/utils/proxy.ts
@@ -1,101 +1,58 @@
-import { BrowserContext, Page } from "playwright";
+import { BrowserContext, Page, Route } from "playwright";
+
+const handleProxy = async (context: Page | BrowserContext, tunnelUrl: URL, route: Route) => {
+	try {
+		const urlObj = new URL(route.request().url());
+		Object.assign(urlObj, { host:  tunnelUrl.host, protocol: tunnelUrl.protocol, port: tunnelUrl.port });
+		(route.request() as any)._initializer.url = urlObj.toString();
+
+		const response = await context.request.fetch(route.request());
+		const responseHeaders = response.headers();
+		delete responseHeaders["content-security-policy"];
+
+
+		return route.fulfill({response, headers: responseHeaders });
+	} catch(err) {
+		console.error("[ProxyMiddleware] Encountered error while intercepting urls", err);
+	}
+}
+
+const setupProxyMiddleware = async (context: Page | BrowserContext, tunnelConfigMap: { [key: string]: { tunnel: string; intercept: { regex: string } & string} }) => {
+	const findTunnelWithPort = (port: number) => {
+		return Object.values(tunnelConfigMap).find(tunnel => tunnel.intercept.includes(`:${port}`));
+	}
+
+	for (let [tunnelName, tunnelConfig] of Object.entries(tunnelConfigMap)) {
+		const matchUrl = !!tunnelConfig.intercept.regex ? eval(`new RegExp(${tunnelConfig.intercept.regex})`)
+						 : (url) => (url.toString().includes(tunnelConfig.intercept));
+		await context.route(
+			matchUrl,
+			handleProxy.bind(this, context, new URL(tunnelConfig.tunnel)),
+		);
+
+		await context.route((url) => (!!url.toString().match(new RegExp(/trycloudflare\.com\:(\d+)/gm))), (route) => {
+			const matches = route.request().url().matchAll(/trycloudflare\.com\:(\d+)/gm);
+			const port = matches.next().value[1];
+			if(port == 80 || port == 443) return route.continue();
+			const matchingTunnel = findTunnelWithPort(port);
+
+			return handleProxy(context, new URL(matchingTunnel.tunnel), route);
+		})
+	}
+};
 
 const handleProxyBrowserContext = async (
 	browserContext: BrowserContext,
-	proxyUrlsMap: { [key: string]: { tunnel: string; intercept: string | { regex: string } } },
+	tunnelConfigMap: { [key: string]: { tunnel: string; intercept: { regex: string } & string} }
 ) => {
-	const proxyKeys = Object.keys(proxyUrlsMap);
-	console.log("proxy is", proxyUrlsMap);
-	//@ts-ignore
-	await Promise.all(
-		proxyKeys.map(async (item) => {
-			const proxyConfig = proxyUrlsMap[item];
-			console.log("Regexp", eval(`new RegExp(${(proxyConfig.intercept as any).regex})`));
-			return browserContext.route(
-				(proxyConfig.intercept as any).regex
-					? eval(`new RegExp(${(proxyConfig.intercept as any).regex})`)
-					: (url) => {
-							return url.toString().includes(proxyConfig.intercept as string);
-					  },
-				async (route) => {
-					try {
-						const requestObj = route.request();
-						const routeUrl = route.request().url();
-						const urlObject = new URL(routeUrl);
-
-						const tunnelUrl = new URL(proxyConfig.tunnel);
-						urlObject.host = tunnelUrl.host;
-						urlObject.protocol = tunnelUrl.protocol;
-						urlObject.port = tunnelUrl.port;
-						
-						console.log("URL IS NOW (page) ", urlObject.toString());
-						(requestObj as any)._initializer.url = urlObject.toString();
-						const response = await browserContext.request.fetch(route.request());
-						const responseHeaders = response.headers();
-						delete responseHeaders["content-security-policy"];
-
-						await route.fulfill({
-							// Pass all fields from the response.
-							response,
-	
-							// Force content type to be html.
-							headers: {
-							  ...responseHeaders
-							}
-						  });
-					} catch (err) {
-						console.error("Encountered error", err);
-					}
-				},
-			);
-		}),
-	);
+	return setupProxyMiddleware(browserContext, tunnelConfigMap);
 };
 
-const handleProxyPage = async (page: Page, proxyUrlsMap: { [key: string]: { tunnel: string; intercept: string | { regex: string } } }) => {
-	const proxyKeys = Object.keys(proxyUrlsMap);
-	await Promise.all(
-		proxyKeys.map(async (item) => {
-			const proxyConfig = proxyUrlsMap[item];
-			return page.route(
-				(proxyConfig.intercept as any).regex
-					? eval(`new RegExp(${(proxyConfig.intercept as any).regex})`)
-					: (url) => {
-							return url.toString().includes(proxyConfig.intercept as string);
-					  },
-				async (route) => {
-					try {
-						const requestObj = route.request();
-						const routeUrl = route.request().url();
-						const urlObject = new URL(routeUrl);
-
-						const tunnelUrl = new URL(proxyConfig.tunnel);
-						urlObject.host = tunnelUrl.host;
-						urlObject.protocol = tunnelUrl.protocol;
-						urlObject.port = tunnelUrl.port;
-						
-						console.log("URL IS NOW (page) ", urlObject.toString());
-						(requestObj as any)._initializer.url = urlObject.toString();
-						const response = await page.request.fetch(route.request());
-						const responseHeaders = response.headers();
-						delete responseHeaders["content-security-policy"];
-
-						await route.fulfill({
-							// Pass all fields from the response.
-							response,
-	
-							// Force content type to be html.
-							headers: {
-							  ...responseHeaders
-							}
-						  });
-					} catch (err) {
-						console.error("Encountered error", err);
-					}
-				},
-			);
-		}),
-	);
+const handleProxyPage = async (
+	page: Page,
+	tunnelConfigMap: { [key: string]: { tunnel: string; intercept: { regex: string } & string} }
+) => {
+	return setupProxyMiddleware(page, tunnelConfigMap);
 };
 
 export { handleProxyBrowserContext, handleProxyPage };

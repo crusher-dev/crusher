b2b3bde38dd2614f5318dae83985ef634c77d87e
Himanshu Dixit <hudixt@gmail.com>
2022-07-31 15:15:59 +0000

    Logo and some small style change

--- packages/crusher-app/src/ui/layout/LoadingScreen.tsx
+++ packages/crusher-app/src/ui/layout/LoadingScreen.tsx
@@ -8,6 +8,7 @@ import CrusherBase from "../layout/CrusherBase";
 
 const logoStyle = css`
 	//box-shadow: 0 0 22px 12px rgb(153 136 255 / 6%);
+	zoom: 1.3;
 	animation: logo-animation 0.6s alternate infinite cubic-bezier(0, 0, 1, 0.32);
 `;
 
@@ -18,7 +19,7 @@ export const LoadingScreen = () => {
 			<CenterLayout className={"pb-100"}>
 				<div className="flex flex-col justify-center items-center">
 					<Logo showOnlyIcon={true} height={"36rem"} css={logoStyle} />
-					<span className={"mt-30 text-14 font-content font-500"}>Firing up all the boosters</span>
+					<span className={"mt-16 text-14 font-content font-500"} css={css`letter-spacing: .3px;`}>Firing up all the boosters</span>
 				</div>
 			</CenterLayout>
 		</CrusherBase>

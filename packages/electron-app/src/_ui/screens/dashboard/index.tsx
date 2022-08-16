c0b13de6a4d38f6313c9659135a31baecf0c99a0
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-08-16 20:51:55 +0530

    (EA) Fix dashboard title alignment

--- packages/electron-app/src/_ui/screens/dashboard/index.tsx
+++ packages/electron-app/src/_ui/screens/dashboard/index.tsx
@@ -104,6 +104,10 @@ const titleStyle = css`
 
 	display: flex;
 	align-items: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 `;
 const DashboardScreen = () => {
     const {userInfo, projects} = useUser();

e78e4f1306b2bd6d44b91b2322d04f7fc862f09e
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-08-18 15:17:30 +0530

    (CLI) Add --version flag

--- packages/cli/src/bin/index.ts
+++ packages/cli/src/bin/index.ts
@@ -11,38 +11,43 @@ const nodeVersion = process.version.match(/^v(\d+\.\d+)/)[1];
 if (parseFloat(nodeVersion) >= 10.0) {
   const args = process.argv.slice(2);
   const helpArgs = ["-h", "--h", "help", "--help", "-help"];
-  console.log(`\n🦖  Crusher v${require("../../package.json").version} \n`);
+  const cliVersion = require("../../package.json").version;
+
+  console.log(`\n🦖  Crusher v${cliVersion} \n`);
 
   const isDefaultCommand = args.length === 0 || ["open", "."].some((x) => args && args[0] === x);
-  if (isDefaultCommand) {
-    // console.log("Choose a command to run");
-    // new EntryCommand().help();
+  if(["version", "--version"].includes(args[0])) {
+    // Do nothing since version gets printed for every command
+  } else { 
+    if (isDefaultCommand) {
+      // console.log("Choose a command to run");
+      // new EntryCommand().help();
 
-    new Promise(async () => {
+      new Promise(async () => {
 
-      // @Todo: Add support for flag token here
-      await loadUserInfoOnLoad({token: undefined});
-      await installCrusherRecorder();
-      await makeSureSetupIsCorrect(null, true);
+        // @Todo: Add support for flag token here
+        await loadUserInfoOnLoad({token: undefined});
+        await installCrusherRecorder();
+        await makeSureSetupIsCorrect(null, true);
 
-      const projectConfigPath = getProjectConfigPath();
-      const projectConfig = getProjectConfig();
+        const projectConfigPath = getProjectConfigPath();
+        const projectConfig = getProjectConfig();
 
-      // let noProjectFlags = "";
-      // if (!projectConfig) {
-      //   const suggestedProjectName = await getProjectNameFromGitInfo();
-      //   noProjectFlags = "--suggested-project-name=" + suggestedProjectName;
-      // }
-      const customFlags = projectConfig && projectConfig.project ? `--project-config-file=${projectConfigPath} --projectId=${projectConfig.project}` : "";
-      
-      execSync(`${getRecorderDistCommand()} --crusher-cli-path=${eval("__dirname") + "/index.js"} ${customFlags} --no-sandbox`, {stdio: "inherit"});
-    });
-  } else if(helpArgs.includes(args[0])) {
-    new EntryCommand().help();
-  } else {
-    new EntryCommand().run();
+        // let noProjectFlags = "";
+        // if (!projectConfig) {
+        //   const suggestedProjectName = await getProjectNameFromGitInfo();
+        //   noProjectFlags = "--suggested-project-name=" + suggestedProjectName;
+        // }
+        const customFlags = projectConfig && projectConfig.project ? `--project-config-file=${projectConfigPath} --projectId=${projectConfig.project}` : "";
+        
+        execSync(`${getRecorderDistCommand()} --crusher-cli-path=${eval("__dirname") + "/index.js"} ${customFlags} --no-sandbox`, {stdio: "inherit"});
+      });
+    } else if(helpArgs.includes(args[0])) {
+      new EntryCommand().help();
+    } else {
+      new EntryCommand().run();
+    }
   }
-
 } else {
   console.error(
     "Node version must be >= 10.0.0. You are using version: " +

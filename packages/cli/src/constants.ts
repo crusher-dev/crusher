1ac46528f5d4ac7339b68a03df8cd2b6696d0a8c
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-08-25 12:21:30 +0530

    WIP

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,8 +1,8 @@
 import * as path from "path";
 import { getIsArm, getRuntimeEnv } from "./utils/utils";
 
-export const BACKEND_SERVER_URL = "http://localhost:8000";
-export const FRONTEND_SERVER_URL = "http://localhost:3000";
+export const BACKEND_SERVER_URL = "https://backend.crusher.dev";
+export const FRONTEND_SERVER_URL = "https://app.crusher.dev";
 
 export const APP_DIRECTORY =
   getRuntimeEnv().CRUSHER_GLOBAL_DIR ||

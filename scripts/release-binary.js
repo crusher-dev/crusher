fd5493fd66b9077af273feb847a24fabf0ba46c0
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-08-31 13:53:53 +0530

    WIP

--- scripts/release-binary.js
+++ scripts/release-binary.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-
+const child_process = require("child_process");
 const ARTIFACTS_PATH = path.resolve(process.cwd(), "./packages/electron-app/bin");
 
 (async () => {
     const artifacts = fs.readdirSync(ARTIFACTS_PATH);
-    console.log(artifacts);
+    
+    for(let artifact of artifacts)
+        child_process.execSync("unzip " = artifact);
 })();

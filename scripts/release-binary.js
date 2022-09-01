34c855d160bbf4befcb4618e465ee7c2b69f953a
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-09-01 06:09:51 +0530

    WIP

--- scripts/release-binary.js
+++ scripts/release-binary.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const child_process = require("child_process");
-const ARTIFACTS_PATH = path.resolve(process.cwd(), "./packages/electron-app/bin");
+const ARTIFACTS_PATH = path.resolve(process.cwd(), "./artifacts");
 
 (async () => {
+    const distPath = path.resolve(ARTIFACTS_PATH, "dist");
+    if(fs.existsSync(distPath)) {
+        fs.rmdirSync(distPath, {recursive: true});
+    }
+
     const artifacts = fs.readdirSync(ARTIFACTS_PATH);
-    
-    for(let artifact of artifacts)
-        child_process.execSync("unzip " = artifact);
+    process.chdir(ARTIFACTS_PATH);
+
+    for(let artifact of artifacts) {
+        console.time("Extracting " + artifact);
+        child_process.execSync("unzip " + artifact + " -d dist");
+        console.timeEnd("Extracting " + artifact);
+    }
 })();

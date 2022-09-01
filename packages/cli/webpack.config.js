cb74ba555e1b062e6866a4ca26c016953ebb888d
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-09-01 10:52:35 +0530

    WIP

--- packages/cli/webpack.config.js
+++ packages/cli/webpack.config.js
@@ -13,13 +13,13 @@ console.log( glob.sync("./src/**/*.ts").reduce(function (obj, el) {
 
 
 const environmentVariables = {
-  'process.env.DOWNLOADS_REPO_URL': 'https://github.com/crusherdev/crusher-downloads',
+  'process.env.DOWNLOADS_REPO_URL': JSON.stringify('https://github.com/crusherdev/crusher-downloads'),
 };
 if(process.env.RECORDER_VERSION) {
-  environmentVariables['process.env.RECORDER_VERSION'] = process.env.RECORDER_VERSION;
+  environmentVariables['process.env.RECORDER_VERSION'] = JSON.stringify(process.env.RECORDER_VERSION);
 }
 if(process.env.DOWNLOADS_REPO_URL) {
-  environmentVariables['process.env.DOWNLOADS_REPO_URL'] = process.env.DOWNLOADS_REPO_URL;
+  environmentVariables['process.env.DOWNLOADS_REPO_URL'] = JSON.stringify(process.env.DOWNLOADS_REPO_URL);
 }
 
 module.exports = {

7a21863ecb64e6a466db38794db8474bcf24e642
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-09-08 18:14:45 +0530

    Fix download-binaries.js

--- scripts/download-binaries.sh
+++ scripts/download-binaries.sh
@@ -14,5 +14,5 @@ if [ "$(uname)" = "Darwin" ] && ! [ -x "$(command -v wget)" ]; then
 fi
 
 echo 'Downloading latest binaries...'
-curl -L --retry 3 --max-time 900 --connect-timeout 60 -s https://api.github.com/repos/crusherdev/celectron-releases/releases/latest  | grep "celectron.*.zip" | cut -d : -f 2,3 | tr -d \" | wget -qi - -cP packages/electron-app/bin --show-progress
+curl -L --retry 3 --max-time 900 --connect-timeout 60 -s https://api.github.com/repos/crusher-dev/celectron-releases/releases/latest  | grep "celectron.*.zip" | cut -d : -f 2,3 | tr -d \" | wget -qi - -cP packages/electron-app/bin --show-progress
 cd packages/electron-app && pnpm extract:deps

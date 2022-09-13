8e7f120b458b87cb9877a375f15aa443cdcece8e
Himanshu Dixit <hudixt@gmail.com>
2022-09-13 22:02:08 +0000

    .

--- packages/crusher-app/pages/app/projects.tsx
+++ packages/crusher-app/pages/app/projects.tsx
@@ -1,8 +1,6 @@
 import { css } from "@emotion/react";
 import React, { useMemo } from "react";
 
-import { OnboardingSteps } from "@ui/containers/dashboard/onboardingSteps";
-import { OnBoardingTutorialVideo } from "@ui/containers/dashboard/tutorials";
 import { SidebarTopBarLayout } from "@ui/layout/DashboardBase";
 
 import { usePageTitle } from "../../src/hooks/seo";
@@ -11,6 +9,8 @@ import { useAtom } from "jotai";
 import { currentProjectSelector } from "../../src/store/selectors/getCurrentProject";
 import { userAtom } from "../../src/store/atoms/global/user";
 import { getOnboardingStepIndex } from "@utils/core/dashboard/onboardingUtils";
+import Input from "dyson/src/components/atoms/input/Input";
+import { Button } from "dyson/src/components/atoms";
 
 function Dashboard() {
 	usePageTitle("Dashboard");
@@ -24,29 +24,79 @@ function Dashboard() {
 
 	return (
 		<SidebarTopBarLayout>
-			<div css={containerStyle} className=" pt-42 ">
+			<div css={containerStyle} className=" pt-36 ">
 				<Conditional showIf={onboardingIndex !== -1}>
-					<div>ds</div>
+					<div className="flex items-center">
+						<Input css={newInputBoxCSS} size={"medium"} placeholder={"Searh project"}></Input>
+						<Button css={buttonCss} size="big-medium" className="ml-16">
+							+ new project
+						</Button>
+					</div>
 				</Conditional>
 			</div>
 		</SidebarTopBarLayout>
 	);
 }
 
+export const newInputBoxCSS = css`
+	flex: 1;
+	input {
+		background: transparent;
+		border: 0.5px solid rgba(56, 56, 56, 0.6);
+		border-radius: 10rem;
+		font-weight: 500;
+		:focus {
+			// background: #121316;
+			border: 0.5px solid #ae47ff;
+			border-color: #ae47ff;
+		}
+		::placeholder {
+			color: #808080;
+		}
+		:hover {
+			// box-shadow: 0px 0px 0px 3px rgba(28, 28, 28, 0.72);
+		}
+	}
+
+	@-webkit-keyframes autofill {
+		0%,
+		100% {
+			color: #666;
+			background: transparent;
+		}
+	}
+
+	input:-webkit-autofill {
+		-webkit-animation-delay: 1s; /* Safari support - any positive time runs instantly */
+		-webkit-animation-name: autofill;
+		-webkit-animation-fill-mode: both;
+	}
+`;
+
+const buttonCss = css`
+	width: 144rem;
+
+	border-radius: 10rem;
+
+	background: #cecece;
+	color: #202020;
+	border-color: transparent !important;
+
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0) !important;
+
+	:hover {
+		background: #eeeeee;
+		color: #202020;
+		filter: brightness(80%);
+	}
+
+	:focus {
+		outline: 1px solid #ae47ff;
+	}
+`;
+
 const containerStyle = css`
 	color: #fff !important;
-`;
-const headingStyle = css`
-	color: #d0d0d0 !important;
-	font-weight: bold;
-`;
-const footerContainerStyle = css`
-	border: 1rem solid #191e25;
-	height: 280rem;
-	border-radius: 6rem;
-`;
-const footerPlaceholderStyle = css`
-	color: rgba(255, 255, 255, 0.5);
 `;
 
 export default Dashboard;

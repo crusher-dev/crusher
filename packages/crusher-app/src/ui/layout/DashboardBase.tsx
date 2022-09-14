b0b57cd9f84b4a069d0da1ee8d0d1399996d0b7a
Himanshu Dixit <hudixt@gmail.com>
2022-09-14 14:31:27 +0000

    .

--- packages/crusher-app/src/ui/layout/DashboardBase.tsx
+++ packages/crusher-app/src/ui/layout/DashboardBase.tsx
@@ -316,7 +316,7 @@ function Chat(props) {
 		<svg width={14} height={14} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
 			<path
 				d="M12.17 1H1.83c-.7 0-1.292.6-1.292 1.31v7.2c0 .709.592 1.308 1.293 1.308h3.135l1.874 2.11a.214.214 0 00.32 0l1.874-2.11h3.135c.7 0 1.292-.6 1.292-1.309v-7.2C13.461 1.6 12.87 1 12.17 1z"
-				fill="#B0B0B0"
+				fill="#838383"
 			/>
 		</svg>
 	);
@@ -359,7 +359,7 @@ function LeftSection() {
 					<div className="flex" css={inviteBoxCSS}>
 						<NewPeople />
 						<div className="ml-6">
-							<TextBlock color="#BC66FF" fontSize={14} weight={600} id="invite">
+							<TextBlock color="#BC66FF" fontSize={13} weight={600} id="invite">
 								Invite People
 							</TextBlock>
 							<TextBlock color="#3E3E3E" fontSize={12} className="mt-4">
@@ -375,7 +375,7 @@ function LeftSection() {
 						</div>
 						<div className="flex items-center" css={feedbackCSS}>
 							<Chat className="mr-8" />
-							<TextBlock fontSize={13} color="#B0B0B0">
+							<TextBlock fontSize={12} color="#838383">
 								Give Feedback
 							</TextBlock>
 						</div>

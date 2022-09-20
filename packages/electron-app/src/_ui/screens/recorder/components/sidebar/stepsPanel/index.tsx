014adbdb979f1a7b77fe349e0ec90f11515c8f41
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-09-20 18:20:13 +0530

    WIP

--- packages/electron-app/src/_ui/screens/recorder/components/sidebar/stepsPanel/index.tsx
+++ packages/electron-app/src/_ui/screens/recorder/components/sidebar/stepsPanel/index.tsx
@@ -5,33 +5,51 @@ import { ConsoleIcon } from 'electron-app/src/ui/icons';
 import { useSelector } from "react-redux";
 import { getIsStatusBarVisible, getSavedSteps } from 'electron-app/src/store/selectors/recorder';
 import { Step } from './step';
+import { useSelectableList } from 'electron-app/src/_ui/hooks/list';
+import { OnOutsideClick } from '@dyson/components/layouts/onOutsideClick/onOutsideClick';
 
 interface IProps {
     className?: string;
 }
 
 const StepsPanel = ({ className, ...props}: IProps) => {
+    const { isItemSelected, selectedList, resetSelected, toggleSelectItem, selectItem, toggleSelectAll } = useSelectableList();
 	const recordedSteps = useSelector(getSavedSteps);
 	const isStatusBarVisible = useSelector(getIsStatusBarVisible);
-
     const toggleStatusBar = React.useCallback(() => {
         
     }, []);
+
+    const handleStepClick = React.useCallback((stepId) => {
+        toggleSelectItem(stepId);
+    }, [toggleSelectItem]);
     const steps = React.useMemo(() => {
         return recordedSteps.map((step, index) => {
             return (
                 <Step
+                    onClick={handleStepClick.bind(this, index)}
+                    isActive={isItemSelected(index)}
+                    setIsActive={selectItem.bind(this, index)}
                     stepId={index}
                 />
             )
         })
-    }, [recordedSteps]);
+    }, [selectedList, selectItem, handleStepClick, recordedSteps]);
 
     React.useEffect(() => {
 		const testListContainer: any = document.querySelector("#steps-list-container");
 		const elementHeight = testListContainer.scrollHeight;
 		testListContainer.scrollBy(0, elementHeight);
 	}, [recordedSteps.length]);
+
+
+    const handleOutSideClick = React.useCallback(() => {
+        // @Note: setTimeOut is here as an hack, to
+        // allow selectedList to be sent to contextMenu onClick
+        setTimeout(() => {
+            resetSelected();
+        }, 100);
+    }, [resetSelected]);
 
     return (
         <div css={containerCss} className={`${className}`}>
@@ -41,9 +59,11 @@ const StepsPanel = ({ className, ...props}: IProps) => {
                     <ConsoleIcon onClick={toggleStatusBar} css={[consoleIconCss, isStatusBarVisible ? consoleActiveIconCss : null]} />
                 </div>
             </div>
-            <div id={"steps-list-container"} className={`custom-scroll`} css={contentCss}>
-                    {steps}
-            </div>
+            <OnOutsideClick className={"custom-scroll"} css={css`height: 100%;`} onOutsideClick={handleOutSideClick}>
+                <div id={"steps-list-container"} className={`custom-scroll`} css={contentCss}>
+                        {steps}
+                </div>
+            </OnOutsideClick>
         </div>
     )
 }

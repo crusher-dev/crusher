32694139cb1f03ef773e4d5740bbc8f1000520f7
Himanshu Dixit <hudixt@gmail.com>
2022-09-22 01:40:10 +0530

    .

--- packages/electron-app/src/_ui/components/stickyFooter.tsx
+++ packages/electron-app/src/_ui/components/stickyFooter.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { css } from "@emotion/react";
 import { CloudIcon, ConsoleIcon, DocsIcon, NotepadIcon } from "../icons";
 import { Link } from "./Link";
+import { TextBlock } from "dyson/src/components/atoms/textBlock/TextBlock";
+import { HoverCard } from "@dyson/components/atoms/tooltip/Tooltip1";
+import { editor } from "monaco-editor";
 
 interface IProps {
     className?: string;
 };
-const StickyFooter = ({className, ...props}: IProps) => {
+const StickyFooter = ({ className, ...props }: IProps) => {
     return (
         <div css={containerCss} className={`${className}`}>
             <div css={contentCss}>
                 <div css={notificationContainerCss}>
                     <div css={notificationContentCss}>
-                        <ConsoleIcon css={consoleIconCss}/>
+                        <ConsoleIcon css={consoleIconCss} />
                         <span css={notificationTextCss}>2: Last build has passed</span>
                     </div>
                     <div css={notificationActionCss}>
@@ -20,31 +23,37 @@ const StickyFooter = ({className, ...props}: IProps) => {
                     </div>
                 </div>
                 <div css={contextContainerCss}>
-                    <CloudIcon css={[cloudIconCss, clickableCss]} shouldAnimateGreen={true}/>
-                    <NotepadIcon css={[notepadIconCss, clickableCss]}/>
+                    <CloudIcon css={[cloudIconCss, clickableCss]} shouldAnimateGreen={true} />
+                    <NotepadIcon css={[notepadIconCss, clickableCss]} />
                 </div>
             </div>
-            <div css={docsButtonCss}>
-                <DocsIcon css={docsIconCss}/>
-                <span css={docsButtonTextCss}>Docs & help</span>
-            </div>
+            <HoverCard content={<HelpContent />} placement="top" type="hover" padding={8} offset={0}>
+                <div css={docsButtonCss}>
+                    <DocsIcon css={docsIconCss} />
+                    <span css={docsButtonTextCss}>Docs & help</span>
+                </div>
+            </HoverCard>
         </div>
     )
 };
 
+
+
 const containerCss = css`
-    background: linear-gradient(0deg, rgba(255, 255, 255, 0.04), rgba(255, 255, 255, 0.04)), linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), #161617;
+    background:  #0D0D0E;
     display: flex;
     align-items: center;
     justify-content: center;
+    border-top: 0.5px solid rgba(255, 255, 255, 0.08);
 `;
 const docsButtonCss = css`
-    background: linear-gradient(0deg, rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05)), #0F1010;
-    border-left: 1px solid #242424;
+background: #0F1010;
+border-left: 0.5px solid #242424;
     font-family: 'Cera Pro';
     font-style: normal;
     font-weight: 500;
     font-size: 12px;
+
 
     color: #FFFFFF;
     width: 124px;
@@ -130,3 +139,117 @@ const linkCss = css`
 
 `;
 export { StickyFooter };
+
+
+//  🔴🔴 Do not edit, this is copy of DashboardBase help section
+function HelpContent() {
+    return (
+        <div className=" pt-3 pb-6">
+
+            <a href="https://docs.crusher.dev" target="_blank">
+                <TextBlock fontSize={13.4} color={"#8F8F8F"} css={linkCSS}>
+                    Documentation <ExternalIcon className="ml-3" />
+                </TextBlock>
+            </a>
+            <a href="https://github.com/crusher-dev/crusher" target="_blank">
+                <TextBlock fontSize={13.4} color={"#8F8F8F"} className={"mt-1"} css={linkCSS}>
+                    <GithubSVG height={11} width={11} className={"mr-6"} /> <span className="mt-2">Github</span><ExternalIcon className="ml-4" />
+                </TextBlock>
+            </a>
+            <a href="https://discord.com/invite/dHZkSNXQrg" target="_blank">
+                <TextBlock fontSize={13.4} color={"#8F8F8F"} className={"mt-1"} css={linkCSS}>
+                    <DiscordSVG height={12} width={13} className={"mr-6"} css={discordIcons} /> <span className="mt-1">Discord</span><ExternalIcon className="ml-3" />
+                </TextBlock>
+            </a>
+        </div>
+    );
+}
+
+
+const discordIcons = css`
+margin-left: -1rem;
+`
+
+const linkCSS = css`
+display: flex;
+align-items: center;
+padding-left: 8rem;
+padding-right: 8rem;
+path{
+	fill: #D1D5DB;
+}
+color: #D1D5DB;
+:hover{
+	background: rgba(43, 43, 43, 0.4);
+	color: #BC66FF;
+	path{
+		fill: #BC66FF;
+	}
+}
+height:28rem;
+width: 148rem;
+border-radius: 6px;
+padding-top:1rem;
+
+transition: all 0ms linear;
+
+path, *{
+	transition: all 0ms;
+}
+
+
+`
+
+
+export function ExternalIcon(props: any) {
+    return (
+        <svg
+            width={8}
+            height={8}
+            fill="none"
+            viewBox="0 0 8 8"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M7.32.487c.24.216.26.584.044.824L1.67 7.486c-.216.239-.76.215-1 0-.24-.216-.216-.761 0-1L6.497.53A.583.583 0 017.32.487z"
+                fill="#3C3C3D"
+            />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M.522 1.256a.583.583 0 01.552-.613L6.9.338a.583.583 0 01.613.552l.305 5.825a.583.583 0 01-1.165.061l-.275-5.242-5.242.274a.583.583 0 01-.614-.552z"
+                fill="#3C3C3D"
+            />
+        </svg>
+    );
+}
+
+export const DiscordSVG = (props) => (
+    <svg width={26} height={27} viewBox="0 0 26 27" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <g clipPath="url(#a)">
+            <path
+                d="M22.01 5.776a21.697 21.697 0 0 0-5.292-1.615.08.08 0 0 0-.085.04c-.229.4-.482.92-.66 1.331a20.114 20.114 0 0 0-5.943 0A13.38 13.38 0 0 0 9.36 4.2a.084.084 0 0 0-.084-.04 21.636 21.636 0 0 0-5.293 1.616.075.075 0 0 0-.034.03C.578 10.76-.346 15.592.107 20.365a.087.087 0 0 0 .034.06 21.698 21.698 0 0 0 6.493 3.23c.033.01.07-.002.09-.03.5-.672.947-1.38 1.329-2.126a.08.08 0 0 0-.045-.112 14.302 14.302 0 0 1-2.028-.951.081.081 0 0 1-.008-.137c.136-.1.272-.204.402-.31a.082.082 0 0 1 .084-.011c4.255 1.911 8.862 1.911 13.067 0a.082.082 0 0 1 .085.01c.13.105.267.211.404.311a.081.081 0 0 1-.007.137c-.648.372-1.321.687-2.03.95a.081.081 0 0 0-.044.113c.39.745.837 1.453 1.328 2.125.02.029.057.04.09.03a21.627 21.627 0 0 0 6.503-3.229.082.082 0 0 0 .034-.058c.542-5.519-.908-10.312-3.844-14.562a.066.066 0 0 0-.034-.03ZM8.688 17.46c-1.28 0-2.336-1.157-2.336-2.578 0-1.422 1.035-2.579 2.336-2.579 1.312 0 2.357 1.167 2.337 2.579 0 1.42-1.035 2.578-2.337 2.578Zm8.64 0c-1.281 0-2.337-1.157-2.337-2.578 0-1.422 1.035-2.579 2.337-2.579 1.311 0 2.357 1.167 2.336 2.579 0 1.42-1.025 2.578-2.336 2.578Z"
+                fill="#5865F2"
+            />
+        </g>
+        <defs>
+            <clipPath id="a">
+                <path fill="#fff" transform="translate(0 .91)" d="M0 0h26v26H0z" />
+            </clipPath>
+        </defs>
+    </svg>
+);
+
+export const GithubSVG = function (props) {
+    return (
+        <svg width={16} height={16} viewBox={"0 0 16 16"} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path
+                d="M8 0a8 8 0 0 0-2.529 15.591c.4.074.529-.174.529-.384v-1.49c-2.225.484-2.689-.944-2.689-.944-.364-.924-.888-1.17-.888-1.17-.726-.497.055-.486.055-.486.803.056 1.226.824 1.226.824.713 1.223 1.871.87 2.328.665.071-.517.279-.87.508-1.07-1.777-.203-3.645-.889-3.645-3.953 0-.874.313-1.588.824-2.148-.082-.202-.356-1.016.078-2.117 0 0 .672-.215 2.201.82A7.673 7.673 0 0 1 8 3.868c.68.004 1.365.093 2.004.27 1.527-1.035 2.198-.82 2.198-.82.435 1.102.161 1.916.079 2.117.513.56.823 1.274.823 2.148 0 3.072-1.871 3.749-3.653 3.947.287.248.549.735.549 1.481v2.196c0 .212.128.462.534.384A8.002 8.002 0 0 0 8 0Z"
+                fill="#fff"
+            />
+        </svg>
+    );
+};
--- packages/electron-app/src/_ui/icons.tsx
+++ packages/electron-app/src/_ui/icons.tsx
@@ -1,64 +1,74 @@
 import React from "react";
 
 const CloudIcon = ({ shouldAnimateGreen, ...props }) => {
-    return (
-        <svg viewBox={"0 0 16 11"} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-            {shouldAnimateGreen ? (
-                <linearGradient id="lg" x1="0.5" y1="1" x2="0.5" y2="0">
-                    <stop offset="0%" stop-opacity="1" stop-color="#E42475" />
-                    <stop offset="40%" stop-opacity="1" stop-color="#E42475">
-                        <animate attributeName="offset" values="0;1" repeatCount="indefinite" dur="0.8s" begin="0s" />
-                    </stop>
-                    <stop offset="40%" stop-opacity="0" stop-color="#E42475">
-                        <animate attributeName="offset" values="0;1" repeatCount="indefinite" dur="0.8s" begin="0s" />
-                    </stop>
-                    <stop offset="100%" stop-opacity="0" stop-color="#E42475" />
-                </linearGradient>
-            ) : (
-                ""
-            )}
-            <path
-                d="M12.854 4.47C12.566 1.953 10.504 0 8 0 5.497 0 3.433 1.953 3.147 4.47 1.409 4.47 0 5.932 0 7.735 0 9.538 1.409 11 3.146 11h9.708C14.59 11 16 9.538 16 7.735c0-1.803-1.409-3.265-3.146-3.265Z"
-                fill={shouldAnimateGreen ? "url(#lg)" : "#E42475"}
-                stroke={"#fff"}
-                strokeWidth="0.5"
-            />
-        </svg>
-    );
-};
-
-const DocsIcon = (props) => (
-    <svg
-      viewBox={"0 0 16 16"}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
+  return (
+    <svg viewBox={"0 0 16 11"} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      {shouldAnimateGreen ? (
+        <linearGradient id="lg" x1="0.5" y1="1" x2="0.5" y2="0">
+          <stop offset="0%" stop-opacity="1" stop-color="#E42475" />
+          <stop offset="40%" stop-opacity="1" stop-color="#E42475">
+            <animate attributeName="offset" values="0;1" repeatCount="indefinite" dur="0.8s" begin="0s" />
+          </stop>
+          <stop offset="40%" stop-opacity="0" stop-color="#E42475">
+            <animate attributeName="offset" values="0;1" repeatCount="indefinite" dur="0.8s" begin="0s" />
+          </stop>
+          <stop offset="100%" stop-opacity="0" stop-color="#E42475" />
+        </linearGradient>
+      ) : (
+        ""
+      )}
       <path
-        d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0Zm0 2c1.245 0 2.459.39 3.471 1.115l-1.442 1.442a3.974 3.974 0 0 0-4.058 0L4.529 3.115A5.958 5.958 0 0 1 8 2Zm2 6a1.738 1.738 0 0 1-.031.309 1.99 1.99 0 0 1-1.66 1.66 1.552 1.552 0 0 1-.617 0 1.991 1.991 0 0 1-1.661-1.66 1.55 1.55 0 0 1 0-.617 1.99 1.99 0 0 1 1.66-1.661 1.55 1.55 0 0 1 .618 0 1.99 1.99 0 0 1 1.66 1.66c.02.102.03.205.031.309ZM2 8c0-1.245.39-2.459 1.115-3.471l1.442 1.442a3.974 3.974 0 0 0 0 4.058l-1.442 1.442A5.959 5.959 0 0 1 2 8Zm6 6a5.958 5.958 0 0 1-3.471-1.115l1.442-1.442a3.974 3.974 0 0 0 4.058 0l1.442 1.442A5.958 5.958 0 0 1 8 14Zm4.885-2.529-1.442-1.442a3.974 3.974 0 0 0 0-4.058l1.442-1.442a5.96 5.96 0 0 1 0 6.942Z"
-        fill="#CDCDCD"
+        d="M12.854 4.47C12.566 1.953 10.504 0 8 0 5.497 0 3.433 1.953 3.147 4.47 1.409 4.47 0 5.932 0 7.735 0 9.538 1.409 11 3.146 11h9.708C14.59 11 16 9.538 16 7.735c0-1.803-1.409-3.265-3.146-3.265Z"
+        fill={shouldAnimateGreen ? "url(#lg)" : "#E42475"}
+        stroke={"#fff"}
+        strokeWidth="0.5"
       />
     </svg>
-);
-  
-const NotepadIcon = (props) => (
+  );
+};
+
+
+export function DocsIcon(props: any) {
+  return (
     <svg
-      viewBox={"0 0 14 14"}
+      width={14}
+      height={14}
       fill="none"
+      viewBox="0 0 14 14"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path
-        d="M10.261 3.661a.778.778 0 0 0-1.1 0l-5.5 5.5a.778.778 0 1 0 1.1 1.1l5.5-5.5a.778.778 0 0 0 0-1.1Z"
-        fill="#BDBDBD"
-      />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M0 2.333A2.333 2.333 0 0 1 2.333 0h9.334A2.333 2.333 0 0 1 14 2.333v9.334A2.333 2.333 0 0 1 11.667 14H2.333A2.333 2.333 0 0 1 0 11.667V2.333Zm2.333-.777h9.334c.43 0 .777.348.777.777v9.334c0 .43-.348.777-.777.777H2.333a.778.778 0 0 1-.777-.777V2.333c0-.43.348-.777.777-.777Z"
-        fill="#BDBDBD"
+        d="M2.937.673C3.521.543 4.22.5 5.034.5h3.932c.814 0 1.513.043 2.097.173.591.131 1.104.36 1.504.76.4.4.629.913.76 1.504.13.584.173 1.283.173 2.097v3.932c0 .814-.043 1.513-.173 2.097-.131.591-.36 1.104-.76 1.504-.4.4-.912.629-1.504.76-.584.13-1.283.173-2.097.173H5.034c-.814 0-1.513-.043-2.097-.173-.591-.131-1.104-.36-1.504-.76-.4-.4-.629-.912-.76-1.504C.543 10.479.5 9.78.5 8.966V5.034c0-.814.043-1.513.173-2.097.131-.591.36-1.104.76-1.504.4-.4.913-.629 1.504-.76zM3.75 1.8a.65.65 0 01.65.65v3.29c0 .571.733.806 1.065.341a1.887 1.887 0 013.07 0c.332.465 1.065.23 1.065-.34V2.45a.65.65 0 111.3 0v3.29c0 1.836-2.355 2.59-3.422 1.097a.587.587 0 00-.956 0C5.455 8.331 3.1 7.576 3.1 5.74V2.45a.65.65 0 01.65-.65z"
+        fill="#989898"
       />
     </svg>
+  );
+}
+
+
+
+
+const NotepadIcon = (props) => (
+  <svg
+    viewBox={"0 0 14 14"}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M10.261 3.661a.778.778 0 0 0-1.1 0l-5.5 5.5a.778.778 0 1 0 1.1 1.1l5.5-5.5a.778.778 0 0 0 0-1.1Z"
+      fill="#BDBDBD"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0 2.333A2.333 2.333 0 0 1 2.333 0h9.334A2.333 2.333 0 0 1 14 2.333v9.334A2.333 2.333 0 0 1 11.667 14H2.333A2.333 2.333 0 0 1 0 11.667V2.333Zm2.333-.777h9.334c.43 0 .777.348.777.777v9.334c0 .43-.348.777-.777.777H2.333a.778.778 0 0 1-.777-.777V2.333c0-.43.348-.777.777-.777Z"
+      fill="#BDBDBD"
+    />
+  </svg>
 );
 
 const ConsoleIcon = (props) => (
@@ -168,34 +178,34 @@ const ConsoleIconV3 = (props) => (
 
 const BasketBallIcon = React.forwardRef((props, ref) => (
   <svg
-  viewBox={"0 0 18 18"}
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-  ref={ref}
-  {...props}
->
-  <path
-    d="M15.75 9a6.75 6.75 0 1 1-13.5 0 6.75 6.75 0 0 1 13.5 0Z"
-    stroke="#303030"
-    strokeWidth={1.5}
-  />
-  <path
-    d="M6 10.5c.684.91 1.773 1.5 3 1.5s2.316-.59 3-1.5M6.75 7.508V7.5M11.25 7.508V7.5"
-    stroke="#303030"
-    strokeWidth={1.5}
-    strokeLinecap="round"
-  />
-</svg>
+    viewBox={"0 0 18 18"}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    ref={ref}
+    {...props}
+  >
+    <path
+      d="M15.75 9a6.75 6.75 0 1 1-13.5 0 6.75 6.75 0 0 1 13.5 0Z"
+      stroke="#303030"
+      strokeWidth={1.5}
+    />
+    <path
+      d="M6 10.5c.684.91 1.773 1.5 3 1.5s2.316-.59 3-1.5M6.75 7.508V7.5M11.25 7.508V7.5"
+      stroke="#303030"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+    />
+  </svg>
 ));
 
 export {
-    CloudIcon,
-    DocsIcon,
-    NotepadIcon,
-    ConsoleIcon,
-    PointerIcon,
-    EditIcon,
-    RocketIcon,
-    ConsoleIconV3,
-    BasketBallIcon
+  CloudIcon,
+  DocsIcon,
+  NotepadIcon,
+  ConsoleIcon,
+  PointerIcon,
+  EditIcon,
+  RocketIcon,
+  ConsoleIconV3,
+  BasketBallIcon
 }

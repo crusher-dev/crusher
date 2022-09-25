2d37ab50e6e1d8bcf36d77618cd487cf2db794b3
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-09-25 16:08:38 +0530

    WIP

---
+++ packages/electron-app/src/store/reducers/builds.tsx
@@ -0,0 +1,40 @@
+import { AnyAction } from "redux";
+
+export interface ICurrentBuildPayload {
+    id: string;
+    tests: Array<any>;
+    queuedTests: Array<any>;
+    time: any;
+};
+
+export interface IBuildResultPayload {
+    status: "PASSED" | "FAILED" | "RUNNING";
+    result?: any;
+};
+
+interface IBuildsReducer {
+    builds: {
+        [id: string]: IBuildResultPayload
+    },
+	currentBuild: ICurrentBuildPayload | null;
+}
+
+const initialState: IBuildsReducer = {
+	builds: {},
+    currentBuild: null,
+};
+
+const buildsReducer = (state: IBuildsReducer = initialState, action: AnyAction) => {
+	switch (action.type) {
+        case TRIGGER_LOCAL_BUILD:
+            return { ...state, currentBuild: action.payload }
+        case CLEAR_CURRENT_LOCAL_BUILD:
+            return { ...state, currentBuild: null };
+        case UPDATE_LOCAL_BUILD_RESULT:
+            return { ...state, builds: { ...state.builds, [action.payload.id]: action.payload } };
+		default:
+			return state;
+	}
+};
+
+export { IBuildsReducer, buildsReducer };
--- packages/electron-app/src/store/reducers/index.tsx
+++ packages/electron-app/src/store/reducers/index.tsx
@@ -3,12 +3,14 @@ import { appReducer, IAppReducer } from "./app";
 import { loggerReducer, ILoggerReducer } from "./logger";
 import { IOnboardingState, onboardingReducer } from "./onboarding";
 import { IRecorderReducer, recorderReducer } from "./recorder";
+import { buildsReducer, IBuildsReducer} from "./builds";
 
 export interface iReduxState {
 	onboarding: IOnboardingState;
 	recorder: IRecorderReducer;
 	app: IAppReducer;
 	logger: ILoggerReducer;
+	builds: IBuildsReducer;
 }
 
 // Use ES6 object literal shorthand syntax to define the object shape
@@ -17,6 +19,7 @@ const rootReducer: Reducer<CombinedState<iReduxState>, AnyAction> = combineReduc
 	recorder: recorderReducer,
 	app: appReducer,
 	logger: loggerReducer,
+	builds: buildsReducer,
 });
 
 export { rootReducer };

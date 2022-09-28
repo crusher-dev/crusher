41010ff1a7a84420287523f6e832a90ae41325bf
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-09-28 11:51:07 +0530

    WIP

--- packages/electron-app/src/ui/components/status-bar/index.tsx
+++ packages/electron-app/src/ui/components/status-bar/index.tsx
@@ -388,7 +388,6 @@ const StatusBar = (props: any) => {
 										css={css`
 											width: 10rem;
 											height: 10rem;
-											opacity: 0.44;
 										`}
 									/>
 								</div>

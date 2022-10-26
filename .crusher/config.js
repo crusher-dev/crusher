d26cbe73aaf5eff0af82c25c0bc1fc737b6e19dc
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-10-27 01:12:45 +0530

    (TE) Remove limiter

--- .crusher/config.js
+++ .crusher/config.js
@@ -1,14 +1,4 @@
 module.exports = {
     "backend": "https://backend.crusher.dev/",
     "project": 258,
-    "env": {
-        "BASE_URL": "http://localhost:3000"
-    },
-    "proxy": [
-        {
-            "name": "frontend",
-            "url": "http://localhost:3000",
-            "intercept": "localhost:3000"
-        },
-    ]
 }

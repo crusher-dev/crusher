565b2e13defeed1ed4e7fc4e468185ac65cd6a1c
Himanshu Dixit <hudixt@gmail.com>
2022-11-09 16:06:25 +0530

    (cli): Add padded string

--- packages/cli/src/utils/cliMessages.ts
+++ packages/cli/src/utils/cliMessages.ts
@@ -1,5 +1,15 @@
+let paddedString = (len) => Array.from({ length: len }, () => ' ').join('');
+
+let padString = (string, desiredLength) => {
+  const len = string.length; const initialLen = Math.floor((desiredLength - len) / 2);
+  const rightLen = desiredLength - (len + initialLen);
+  if (len > desiredLength) return string.substring(0, desiredLength);
+  return paddedString(initialLen) + string + paddedString(rightLen);
+};
+
+
 export const Message = (chalk, firstMessage, secondMessage, shouldShowInEveryService: boolean = false) => {
-	console.log(chalk(firstMessage), ' ', secondMessage + (shouldShowInEveryService ? " " : ""));
+	console.log(chalk(padString(firstMessage,9)), ' ', secondMessage + (shouldShowInEveryService ? " " : ""));
 };
 
 export const BlankMessage = (firstMessage, shouldShowInEveryService: boolean = false) => {

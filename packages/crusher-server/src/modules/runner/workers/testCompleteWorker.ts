4673fbaf2dd0393f53f84bf05c69d5b7a27b637f
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-12-13 20:29:30 +0530

    .

--- packages/crusher-server/src/modules/runner/workers/testCompleteWorker.ts
+++ packages/crusher-server/src/modules/runner/workers/testCompleteWorker.ts
@@ -4,6 +4,7 @@ import Container from "typedi";
 import { ITestCompleteQueuePayload } from "@crusher-shared/types/queues/";
 import { TestRunnerQueue } from "../queue.service";
 
+const runnerQueueService = Container.get(TestRunnerQueue);
 
 interface ITestResultWorkerJob extends Job {
 	data: ITestCompleteQueuePayload;
@@ -31,8 +32,6 @@ function setupGracefulShutdown() {
 
 setupGracefulShutdown();
 const processTestAfterExecution = async function (bullJob: ITestResultWorkerJob): Promise<any> {
-	const runnerQueueService = Container.get(TestRunnerQueue);
-
 	return runnerQueueService.handleWorkerJob(bullJob);
 };
 
